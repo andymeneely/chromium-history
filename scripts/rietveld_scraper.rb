4299c79ffa8248f1f6c0a37b17423cf024124599
Katherine Whitlock <toroidalcode@gmail.com>
2013-09-20 22:37:01 -0400

    Updated scraper

--- scripts/rietveld_scraper.rb
+++ scripts/rietveld_scraper.rb
@@ -1,57 +1,104 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'oj'
-require 'pp'
 require 'set'
+require 'msgpack'
 
-baseurl = 'https://codereview.chromium.org'
+class RietveldScraper
+  attr_accessor :ids
 
-def get_json_response(url)
-  Oj.load(Net::HTTP::get(URI.parse(url)))
-end
+  @@baseurl = 'https://codereview.chromium.org'
 
-def get_ids(cursor=nil)
-  Net::HTTP::post_form( 
-    URI.parse("https://codereview.chromium.org/search"), 
+  def self.get_json_response(url, args=nil)
+    Oj.load(Net::HTTP::get(URI.parse(url, args)))
+  end
+
+  def self.get_more_ids(cursor=nil)
+    Net::HTTP::post_form( 
+      URI.parse(@@baseurl + "/search"), 
+      {
+        "closed" => "1",
+        "private" => "1", 
+        "commit" => "1",
+        "format" => "json",
+        "keys_only" => "True",
+        "with_messages" => "False",
+        "cursor" => "#{cursor}"
+      }
+    )
+  end
+
+  def initialize
+    @cursor = nil
+    @ids = Set.new
+    @issues = Array.new
+    @patches = Array.new
+  end
+
+  def to_hash
     {
-      "closed" => "1",
-      "private" => "1", 
-      "commit" => "1",
-      "format" => "json",
-      "keys_only" => "True",
-      "with_messages" => "False",
-      "cursor" => "#{cursor}"
+      cursor:  @cursor,
+      ids:     @ids.to_a,
+      issues:  @issues,
+      patches: @patches
     }
-  )
+  end
+  
+  def to_s
+    puts @ids.first
+  end
+
+  def to_json
+    Oj.dump(self.to_hash)
+  end
+
+  def to_msgpack(io=nil)
+    if io
+      io.write self.to_hash
+    else
+      MessagePack.pack self.to_hash
+    end
+  end
+
+  def get_ids
+    puts "Fetching IDs:"
+    while true
+      begin
+        json = Oj.load(self.get_more_ids(@cursor).body)
+        @cursor = json['cursor']  # Grab the cursor from the response
+        new_ids = json['results']  # Grab the array of new ids from the response
+        new_ids.each do |id|  # For each of the new ids we've fetched...
+          if ids.include? id  # Check if what we have is already in our Set
+            puts "We're fetching stale ids. Stopping."
+            break
+          else  # If it's not, let's push it on
+            @ids << id 
+          end
+        end
+        print '.'  # A way to tell how many responses we've dealt with
+      rescue
+        puts "Reached end of results"
+      end
+    end
+  end
+
+  def get_data
+    puts "Fetching Data:\n"
+    if not @ids  # let's make sure we've got some ids
+      get_ids
+    end
+
+    ids.map do |id|
+      print '.'
+      @issues << self.get_json_response(baseurl + "/api/#{id}", {
+        "messages" => true
+        })
+    end
+  end
+
 end
 
-cursor = nil
-ids = Set.new()
-
-1.times do
-  p '.'
-  response = get_ids(cursor)
-  json = Oj.load(response.body)
-  cursor = json['cursor']
-  new_ids = json['results']
-  new_ids.each { |id| ids << id }
-  # ids.collect do |id|
-  # #   Oj.load(Net::HTTP::get(URI.parse "https://codereview.chromium.org/api/#{id}"))
-  # end
-end
-
-p ids
-
-data = ids.to_a[0..5].collect do |id|
-  p '.'
-  get_json_response(baseurl + "/api/#{id}")
-end
-
-# f =File.open('data.txt', 'w') 
-# f << data
-
-element = data[0]
-puts "Latest issue: #{element}" 
-puts "Issue Id: #{element['issue']}"
-puts "Patchsets: #{element['patchsets']}"
-puts "First Patchset:" + get_json_response(baseurl + "/api/#{element['issue']}/#{element['patchsets'][0]}").to_s
+# puts "Latest issue: #{element}" 
+# puts "Issue Id: #{element['issue']}"
+# puts "Patchsets: #{element['patchsets']}"
+# puts "First Patchset:" + get_json_response(baseurl + "/api/#{element['issue']}/#{element['patchsets'][0]}").to_s
