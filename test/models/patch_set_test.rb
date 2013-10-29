01c7a961bb9960441f1140d821351b73059d6681
Alberto Rodriguez <alberto.c.rodriguez@gmail.com>
2013-10-29 03:01:51 -0400

    Unit tests first draft.

--- test/models/patch_set_test.rb
+++ test/models/patch_set_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PatchSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that patchsets are being parsed correctly" do
+	
+	cr = CodeReview.find_by issue: "23444043"
+	
+    # The loader is not unit testable in its current form.
+	# Should be refactored to receive a mock database and the json documents.
+	CodeReviewLoader.new.load_patchsets("23444043", cr, "20001")
+	
+	ps = PatchSet.where(:patchset => "20001", :code_review_id => cr.id)
+	
+	assert_equal(2, ps.num_comments)
+	assert_equal("Add start refcount + fix potential hang during Stop()",ps.message)
+  end
 end
