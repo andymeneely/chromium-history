b4c0a69df2d9282240cf03d9f08d5f5729ebab94
Katherine Whitlock <toroidalcode@gmail.com>
2013-11-13 12:16:13 -0500

    Modified converter to account for file deletions

--- lib/chromium_history/git_log_to_csv.rb
+++ lib/chromium_history/git_log_to_csv.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 prev_hash = ""
 
-puts "SHA1,Relative File Path"
 ARGF.each_line do |line|
   line.strip
   if m = line.match(/[0-9a-f]{40}/)
     prev_hash = m
-  elsif line != "\n"
+  elsif line != "\n" and not line.start_with?('D') # if we're not a deletion
+    line = line[1..-1].strip
   	puts "#{prev_hash},#{line}"
   end
 end
