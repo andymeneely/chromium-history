77e6d4fed5ba8b9aa01a55557537344912cdfb82
Andy Meneely <andy@se.rit.edu>
2013-11-19 11:13:35 -0500

    Migrating the database

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131119001433) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "code_reviews", force: true do |t|
     t.text     "description"
     t.string   "subject"
