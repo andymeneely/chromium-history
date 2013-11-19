61ef1ffbddf098ac56f9f6d6af12b4427ce30fd0
Danielle Neuberger <drn5447@rit.edu>
2013-11-19 08:52:18 -0500

    patch set review edited

--- lib/chromium_history/verify/development/date_verify.rb
+++ lib/chromium_history/verify/development/date_verify.rb
@@ -12,20 +12,19 @@ class DateVerify < VerifyBase
     end
   end
 
-  #TODO: finish checking patch set dates
-  #
   # Danielle: there is no patchset with this id, 
   #           also remember that patchset numbers are not unique,
   #      E.g. code reviews 10854242 and 23444043 both have a patchset number 1.
   #
-  # def verify_patchset_9141024_created_date
-  #   patchset = PatchSet.where(patchset: 9141024).first
-  #   #remember to convert dates to strings for comparison
-  #   if patchset.created.to_s.eql?("2012-01-20 23:53:25 UTC") then 
-  #     pass()
-  #   else
-  #     fail("Wrong created date on patchset 9141024.")
-  #   end
-  # end
+  def verify_patchset_9141024_created_date
+    #issueID = CodeReview.where(issue: 9141024)
+    patchset = PatchSet.where(code_review_id: 4).first
+    #remember to convert dates to strings for comparison
+    if patchset.created.to_s.eql?("2012-01-20 23:36:18 UTC") then 
+      pass()
+    else
+      fail("Wrong created date on patchset 9141024.")
+    end
+  end
   
 end
