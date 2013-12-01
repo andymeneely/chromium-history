786f20f90b892ddbc37efaa7d325e015f98ae5b6
Andy Meneely <andy@se.rit.edu>
2013-12-01 15:02:10 -0500

    Add index to commit

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,5 +2,6 @@ class Commit < ActiveRecord::Base
   has_many :commit_files, foreign_key: "commit_hash", primary_key: "commit_hash"
   
   def self.on_optimize
+    ActiveRecord::Base.connection.add_index :commits, :commit_hash, unique: true
   end
 end
