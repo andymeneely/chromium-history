1ad86d99d5f97020a39bcd88b79afada09db3794
Andy Meneely <andy@se.rit.edu>
2013-12-05 11:23:38 -0500

    Fixing commit development data script

--- lib/chromium_history/scripts/gitlog-devdata.sh
+++ lib/chromium_history/scripts/gitlog-devdata.sh
@@ -6,7 +6,9 @@ OUTPUT=~/chromium/history/data/development/chromium-gitlog.txt
 
 cd $GIT_SRC
 
+rm $OUTPUT
+
 for COMMIT in $(cat $COMMITS)
 do
-    git log -1 --pretty=format:":::%n%H%n%an%n%ae%n%ad%n%P%n%s%n%b" --stat --stat-width=300 --stat-name-width=300 --ignore-space-change $COMMIT > $OUTPUT
+    git log -1 --pretty=format:":::%n%H%n%an%n%ae%n%ad%n%P%n%s%n%b" --stat --stat-width=300 --stat-name-width=300 --ignore-space-change $COMMIT >> $OUTPUT
 done
