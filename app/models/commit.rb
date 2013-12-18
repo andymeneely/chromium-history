9ad2a8903926151b96520e0afa581a617af0217e
Andy Meneely <andy@se.rit.edu>
2013-12-17 21:32:48 -0500

    Associating Commit and CodeReview, fixes #57

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -4,10 +4,12 @@ class Commit < ActiveRecord::Base
   	has_many :commits_filepaths # For join table assoc
   	has_many :filepaths, :through => :commits_filepaths # For join table assoc
   
+  belongs_to :code_review, :class_name => 'CodeReview', foreign_key: 'code_review_id', primary_key: 'issue'
+
   def self.on_optimize
     ActiveRecord::Base.connection.add_index :commits, :commit_hash, unique: true
     ActiveRecord::Base.connection.add_index :commits, :parent_commit_hash
     ActiveRecord::Base.connection.add_index :commits, :author_email
-
   end
+
 end
