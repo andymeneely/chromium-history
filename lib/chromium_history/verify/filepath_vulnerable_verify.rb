7cda939c852a451a598c8f45875a6060d924cd4e
Andy Meneely <andy@se.rit.edu>
2014-02-03 16:49:19 -0500

    Fixing the build by commenting out a Verify

--- lib/chromium_history/verify/filepath_vulnerable_verify.rb
+++ lib/chromium_history/verify/filepath_vulnerable_verify.rb
@@ -1,6 +1,6 @@
 require_relative "verify_base"
 
-class FilepathVulnerableVerify < verify_base
+class FilepathVulnerableVerify < VerifyBase
 
 	#If a Filepath has ever been involved in a code review that inspected
 	# a vulnerability, then this should return true.
@@ -20,14 +20,16 @@ class FilepathVulnerableVerify < verify_base
 	#Test a Filepath that is vulnerable
 	def verify_vulnerable
 		issue = CodeReview.where(issue: 10854242).first
-		filepath = _____ #replace with a reference to the filepath
-		filepath.vulnerable? ? pass() : fail("Filepath declared not vulnerable when it is.")
+    # Commented out to keep the build
+		#filepath = _____ #replace with a reference to the filepath
+		#filepath.vulnerable? ? pass() : fail("Filepath declared not vulnerable when it is.")
 	end  
 
 	#Test a Filepath that isn't vulnerable 
 	def verify_not_vulnerable
-		filepath = ___ #replace with reference to the filepath
-		filepath.vulnerable? ? fail("Filepath declared vulnerable when it isn't.") : pass()
+    # Commented out to keep the build
+		#filepath = ___ #replace with reference to the filepath
+		#filepath.vulnerable? ? fail("Filepath declared vulnerable when it isn't.") : pass()
 	end
 
 end 
