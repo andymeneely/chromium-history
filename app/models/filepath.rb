b4dee73efeee3366e3f6e294d25a03714a95e8d5
Katherine Whitlock <toroidalcode@gmail.com>
2014-02-06 23:17:34 -0500

    Working jointables

--- app/models/filepath.rb
+++ app/models/filepath.rb
@@ -1,6 +1,6 @@
 class Filepath < ActiveRecord::Base
-	has_many :commits_filepaths# For join table assoc
-	has_many :commits, :through => :commits_filepaths# For join table assoc
+	has_many :commit_filepaths                     # For join table assoc
+	has_many :commits, :through => :commit_filepaths # For join table assoc
 
 	def self.on_optimize
   end
