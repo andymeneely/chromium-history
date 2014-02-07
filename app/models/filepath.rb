d21dc24dc55be4256f44e6f29e3bffdfc20a3593
Katherine Whitlock <toroidalcode@gmail.com>
2014-02-06 20:40:13 -0500

    Add filepath entry creation to loaders
    This fixes #68

--- app/models/filepath.rb
+++ app/models/filepath.rb
@@ -3,12 +3,16 @@ class Filepath < ActiveRecord::Base
 	has_many :commits, :through => :commits_filepaths# For join table assoc
 
 	def self.on_optimize
-  	end
+  end
 
-  	#If a Filepath has ever been involved in a code review that inspected 
-  	#a vulnerability, then this should return true.
-  	def vulnerable?
-  		#self.cve?
- 	end
+  #If a Filepath has ever been involved in a code review that inspected
+  #a vulnerability, then this should return true.
+  def vulnerable?
+    #self.cve?
+  end
+
+  def reviewers
+    # Some stuff here
+  end
 end
 
