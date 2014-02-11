8087239e37757d31e7fa8f3473f9f9a59d4cabfb
Katherine Whitlock <toroidalcode@gmail.com>
2014-02-11 17:59:59 -0500

    Drop reviewers field from commit
    Contributes to #76

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216210846) do
+ActiveRecord::Schema.define(version: 20140211225817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20131216210846) do
     t.string   "author_email"
     t.text     "message"
     t.string   "bug"
-    t.string   "reviewers"
     t.string   "svn_revision"
     t.datetime "created_at"
     t.integer  "commit_files_id"
