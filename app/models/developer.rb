ef4aabbbb57782bbc1aa39d3db7cf3c3c1e305d3
Shannon Trudeau <smt9020@rit.edu>
2014-02-14 13:44:05 -0500

    Associate reviewers with developers
    This association goes through the email addresses, so that you can get
    the developers that reviewed a code review

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,6 @@
 class Developer < ActiveRecord::Base
   has_many :code_reviews
+  belongs_to :reviewer
 
   def self.on_optimize
     ActiveRecord::Base.connection.add_index :developers, :email, unique: true
