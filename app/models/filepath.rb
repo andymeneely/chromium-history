0c91afce2d6ee8c67160c24d376e72021f2bdf48
Andy Meneely <andy@se.rit.edu>
2014-03-14 13:36:05 -0400

    Fixing commit dc7ff79bdd to finish #59
    * Added a new verify for it
    * Added a cves method that utilizes joins in an efficient manner
    * vulnerable? just calls the cves method

--- app/models/filepath.rb
+++ app/models/filepath.rb
@@ -3,20 +3,17 @@ class Filepath < ActiveRecord::Base
   has_many :commit_filepaths, primary_key: 'filepath', foreign_key: 'filepath'
 
 	def self.on_optimize
-    ActiveRecord::Base.connection.add_index :filepaths, :path, unique: true
+    ActiveRecord::Base.connection.add_index :filepaths, :filepath, unique: true
   end
 
   #If a Filepath has ever been involved in a code review that inspected
   #a vulnerability, then this should return true.
   def vulnerable?
-    path = self.path 
-    commit_filepath = CommitFilepath.find_by filepath: path 
-    commit_filepath.cve?
-    
+    cves.any?
+  end
 
-
-
-
+  def cves
+    Filepath.joins(commit_filepaths: [commit: [code_review: :cvenums]]).where(filepath: filepath)
   end
 
   # Delegates to the static method with the where clause
--- lib/chromium_history/verify/development/association_verify.rb
+++ lib/chromium_history/verify/development/association_verify.rb
@@ -8,4 +8,12 @@ class AssociationVerify < VerifyBase
     assert_equal exp, f.reviewers.pluck(:email).sort
   end
 
+  def verify_filepath_vulnerable
+    assert_equal true, Filepath.find_by(filepath: 'ui/base/x/x11_util.cc').vulnerable?
+  end
+
+  def verify_filepath_neutral
+    assert_equal false, Filepath.find_by(filepath: 'content/renderer/media/webrtc_audio_renderer.cc').vulnerable?
+  end
+
 end#end of class
