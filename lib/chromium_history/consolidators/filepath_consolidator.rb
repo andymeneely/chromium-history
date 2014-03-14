63d55023d53b0f081231fc9256766d604a2358c0
Andy Meneely <andy@se.rit.edu>
2014-03-14 13:25:45 -0400

    Reverting the most recent commit to fix this again

--- lib/chromium_history/consolidators/filepath_consolidator.rb
+++ lib/chromium_history/consolidators/filepath_consolidator.rb
@@ -3,12 +3,12 @@ class FilepathConsolidator
   # Given all locations of filepaths that we know of, make one Filepath table
   def consolidate
     query=<<-eos
-    INSERT INTO filepaths (path) 
-      SELECT DISTINCT filepath AS joined_paths FROM(
+    INSERT INTO filepaths (filepath) 
+      SELECT DISTINCT filepath FROM(
         SELECT filepath FROM commit_filepaths
         UNION
         SELECT filepath FROM patch_set_files
-      ) WHERE NOT EXISTS (SELECT * FROM filepaths WHERE path = joined_paths)
+      ) all_filepath 
     eos
     ActiveRecord::Base.connection.execute query
   end
