418c91d0ecf6abc024c08a1cee304e76e31b61d3
Shannon Trudeau <smt9020@rit.edu>
2014-03-21 15:13:44 -0400

    Contributors Table
    Contributors filters now, doesnt check for duplicates yet

--- lib/chromium_history/consolidators/developer_consolidator.rb
+++ lib/chromium_history/consolidators/developer_consolidator.rb
@@ -31,25 +31,29 @@ class DeveloperConsolidator
     ActiveRecord::Base.connection.execute query
 
     # Iterate over the model with batch processing (see ActiveRecord docs) 
-#    Contributors.all.find_in_batches(batch_size: 1000) do |group|
-#      group.each { |contributor| 
-#        issueNumber = contributor.issue
-#        #take the issue number and look up in messages or comments
-#        mess = Message.where("code_review_id = ?", issueNumber)
-#        for m in mess 
-#          txt = m.text
-#          if contribution?(txt)  #this is wrong..
-#            puts m.sender
-#          end
-#        end
-#      }
-#    end
+    Contributor.all.find_in_batches(batch_size: 1000) do |group|
+      group.each { |contributor| 
+        issueNumber = contributor.issue
+        #take the issue number and look up in messages or comments
+        mess = Message.where("code_review_id = ? AND sender = ?", issueNumber, contributor.email)  # and sender is the contributor
+        for m in mess 
+          txt = m.text
+          if !contribution?(txt)  
+            #delete this row of the table
+            Contributor.delete_all(:issue => issueNumber, :email => contributor.email)
+          end
+        end
+      }
+    end
     ##   - delete the record if it's not a contribution
     # 
   end
 
   def contribution?(txt)
-
+    if txt.length > 20
+      #check for duplicates
+      return true
+    end
   end
 
 end
