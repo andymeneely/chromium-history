f5b03990e355c32230fb6f20e2c1bac12c44c671
Chris Ketant <ketantchris@gmail.com>
2014-04-11 03:19:50 -0400

    Added Vulnerability_Inpsections method on a Developer

--- app/models/filepath.rb
+++ app/models/filepath.rb
@@ -17,6 +17,7 @@ class Filepath < ActiveRecord::Base
   end
 
   # Delegates to the static method with the where clause
+  # Does not get the reviewers, returns Filepath object
   def reviewers
     Filepath.reviewers.where(filepath: filepath).uniq
   end
@@ -26,6 +27,37 @@ class Filepath < ActiveRecord::Base
   def self.reviewers
     Filepath.joins(commit_filepaths: [commit: [code_review: :reviewers]])
   end
+
+  # Get the number of developers
+  # who have reviewed vulnerablities up until 
+  # the param date who have also inspected
+  # this Filepath. Does not work b/c self.reviewers
+  # returns Filepath, not reviewers
+  #
+  # @param- date Time object or string. Str format: "DD-MM-YYYY HH:MM:SS"
+  # @return - number of vulnerability developers
+  def num_vulnerable_devs(date=Time.now)
+
+    #if date is a string then convert to Time object
+    if date.class == String then date = Time.new(date) end
+
+    #Get all the reviewers associated w/
+    #this Filepath
+    reviewers = self.reviewers
+    dev_vul_count = 0
+    reviewers.each do |reviewer| 
+      
+      #Get the developer who is this reviewer
+      dev = reviewer.developer
+
+      #if the dev inspected vulnerable reveiews then this is a vulnerability dev
+      if dev.num_vulnerable_inspects(date) > 0 then dev_vul_count+=1 end
+
+    end#loop
+
+    return dev_vul_count
+
+  end#num_of_vulnerable_devs
 
 end
 
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -1,5 +1,7 @@
 class Reviewer < ActiveRecord::Base
-	belongs_to :code_review
+	
+	belongs_to :code_review, foreign_key: 'issue', primary_key: 'issue'
+
 	has_one :developer, foreign_key: "email", primary_key: "email"
 
   def self.on_optimize
