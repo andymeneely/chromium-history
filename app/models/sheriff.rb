f0142a4a77b292099578830a713082c689981c9d
dani5447 <drn5447@rit.edu>
2014-04-26 14:05:09 -0400

    Changed sheriff email to unique

--- app/models/sheriff.rb
+++ app/models/sheriff.rb
@@ -4,7 +4,7 @@ class Sheriff < ActiveRecord::Base
 	 belongs_to :reviewer, primary_key: "email", foreign_key: "email"
 
    def self.on_optimize
-     ActiveRecord::Base.connection.add_index :sheriffs, :email#, unique: true 	#should email be unique? what if there is a 
+     ActiveRecord::Base.connection.add_index :sheriffs, :email, unique: true 	#should email be unique? what if there is a 
      																			#developer who has been sheriff for two different
      																			#types of sheriff events? is that possible?
      ActiveRecord::Base.connection.add_index :sheriffs, :start
