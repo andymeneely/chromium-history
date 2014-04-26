75163e3a1474929f01f0288df9642877351b1b2c
bspates <bxs4361@g.rit.edu>
2014-04-26 14:44:26 -0400

    update verify for new email validation test data

--- lib/chromium_history/verify/development/association_verify.rb
+++ lib/chromium_history/verify/development/association_verify.rb
@@ -4,7 +4,7 @@ class AssociationVerify < VerifyBase
 
   def verify_reviewers_for_filepath
     f = Filepath.where(filepath: 'ui/base/x/x11_util.cc').take 
-    exp = ["agl@chromium.org", "ben@chromium.org", "darin@chromium.org", "derat@chromium.org", "kbr@chromium.org", "sadrul@chromium.org"]
+    exp = ["agl@chromium.org", "ben@chromium.org", "darin@chromium.org", "derat@chromium.org", "dgozmanchromium.org", "kbr@chromium.org", "sadrul@chromium.org"]
     assert_equal exp, f.reviewers.pluck(:email).sort
   end
 
