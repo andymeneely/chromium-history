10c4e7d85859558b02eb94f219913eea0eeb0fa4
Chris Ketant <ketantchris@gmail.com>
2014-04-30 01:40:20 -0400

    Adding verifies files for the vulnerability methods and the num of experienced developers method

---
+++ lib/chromium_history/verify/development/experienced_developers_verify.rb
@@ -0,0 +1,10 @@
+require_relative "../verify_base"
+
+class ExperiencedDevelopersVerify < VerifyBase
+	
+
+#Verify the total number of experienced developers in dev db
+
+#Verify an experienced developer is included in Experienced Developers
+
+end#class
