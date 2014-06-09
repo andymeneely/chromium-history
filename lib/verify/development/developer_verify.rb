3206100b2ee3e1ac1dcd3d95f9b65c033b3d94a5
Kayla Davis <kd9205@rit.edu>
2014-06-09 09:38:12 -0400

    verify for blacklisted_email_domain written & pass

--- lib/verify/development/developer_verify.rb
+++ lib/verify/development/developer_verify.rb
@@ -6,4 +6,15 @@ class DeveloperVerify < VerifyBase
     assert_equal("ben@chromium.org", Developer.where("email like ?","ben%").take[:email],"Email +appenders not being ignored")
   end
 
+  def verify_blacklisted_email_local?
+    assert_equal(true, Developer.blacklisted_email_local?("chromium-reviews"), "chromium-reviews should be blacklisted")
+    assert_equal(true, Developer.blacklisted_email_local?("reply") , "reply should be blacklisted")
+    assert_equal(false, Developer.blacklisted_email_local?("GLaDOS") , "GLaDOS should not be blacklisted")
+  end 
+
+  def verify_blacklisted_email_domain?
+    assert_equal(true, Developer.blacklisted_email_domain?("googlegroups.com"),"googlegroups.com should be blacklisted")
+    assert_equal(false, Developer.blacklisted_email_domain?("aperturescience.gov"), "aperturescience.gov shouldn't be blacklisted")
+  end
+
 end#end of class
