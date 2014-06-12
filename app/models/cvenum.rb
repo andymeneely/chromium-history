c1e904a19de61ed1af55be607a96b36dd1bc30da
Andy Meneely <andy@se.rit.edu>
2014-06-12 13:54:24 -0400

    Enabling indexing again for Cvenum

--- app/models/cvenum.rb
+++ app/models/cvenum.rb
@@ -3,9 +3,8 @@ class Cvenum < ActiveRecord::Base
   self.primary_key = :cve
 
   def self.on_optimize
-  	# FIXME - problem is in cve loader
-    #ActiveRecord::Base.connection.add_index :cvenums, :cve, unique: true
-    #ActiveRecord::Base.connection.add_index :code_reviews_cvenums, [:cvenum_id, :code_review_id], unique: true
+    ActiveRecord::Base.connection.add_index :cvenums, :cve, unique: true
+    ActiveRecord::Base.connection.add_index :code_reviews_cvenums, [:cvenum_id, :code_review_id], unique: true
   end
 
 end
