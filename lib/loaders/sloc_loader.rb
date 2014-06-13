31a02bca23efd1f8ce37c1704034f05a816dc00c
Kayla Davis <kd9205@rit.edu>
2014-06-13 11:53:59 -0400

    fixed error in release_filepath_loader

--- lib/loaders/sloc_loader.rb
+++ lib/loaders/sloc_loader.rb
@@ -2,3 +2,14 @@ require 'csv'
 
 class SlocLoader
 
+  def load
+    datadir =  File.expand_path(Rails.configuration.datadir)
+    CSV.foreach("#{datadir}/sloc/sloc.csv") do |line|
+      if $. == 1 or $. == 2
+        next
+      end
+      match = line[1].match(/([^\.\/].+)/) #removes ./ from pathname
+      out_line = [match[1], line[4]] #filepath, sloc
+    end
+  end
+end
