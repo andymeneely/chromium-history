15e92b6f69aef6929e4a82cb2d0472a4615f407b
Andy Meneely <andy@se.rit.edu>
2014-06-16 16:38:41 -0400

    Updating the chunk script to be smaller

--- lib/scripts/chunkify-code-reviews.rb
+++ lib/scripts/chunkify-code-reviews.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-puts "Moving json code reviews into subdirectories of 5000 each..."
+puts "Moving json code reviews into subdirectories of 250 each..."
 counter = 0
 chunk = 0
 new_chunk = true
@@ -17,7 +17,7 @@ Dir["*.json"].each do |file|
   `mv #{file[0..-6]} #{dir}` #move the directory
 
   counter+=1
-  if counter>=5000
+  if counter>=250
     counter = 0
     chunk += 1
     new_chunk = true
