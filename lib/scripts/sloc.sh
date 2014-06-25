029f18b338181b99c904539213b107a71f858080
Andy Meneely <andy@se.rit.edu>
2014-06-25 15:10:03 -0400

    Hopefully fixes CLOC data being insufficient
    Adding in the --skip-uniqueness because files with different names but
    same content were not getting checked twice. This time CLOC just
    revisits multiple times so we need to do a sort and uniq on the data.

--- lib/scripts/sloc.sh
+++ lib/scripts/sloc.sh
@@ -2,4 +2,9 @@
 
 # outputs language,filename,blank,comment,code
 # into a csv
-cloc --by-file --progress-rate=0 --quiet --csv . > sloc.csv
+#
+# --skip-uniqueness because some files are repeated but with different paths
+# (but then we have to sort and uniq them to remove duplicate lines)
+#
+# Chop off the top two header rows too
+cloc --by-file --progress-rate=0 --quiet --csv --skip-uniqueness . | tail -n +2 | sort | uniq > sloc.csv
