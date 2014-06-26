5b1cbde426da193c41479c5f8561bd0f4e1500ae
Andy Meneely <andy@se.rit.edu>
2014-06-26 09:58:33 -0400

    Fixing the tail command on cloc script

--- lib/scripts/sloc.sh
+++ lib/scripts/sloc.sh
@@ -6,5 +6,6 @@
 # --skip-uniqueness because some files are repeated but with different paths
 # (but then we have to sort and uniq them to remove duplicate lines)
 #
-# Chop off the top two header rows too
-cloc --by-file --progress-rate=0 --quiet --csv --skip-uniqueness . | tail -n +2 | sort | uniq > sloc.csv
+# Chop off the top two header rows too (three newlines)
+cloc --by-file --progress-rate=0 --quiet --csv --skip-uniqueness . | tail -n +3 - | sort | uniq > sloc.csv
+
