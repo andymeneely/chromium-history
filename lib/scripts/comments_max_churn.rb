96e0569edb1c8f0386e0f0ee05c079a7cc7f55d1
Kayla Davis <kd9205@rit.edu>
2014-07-11 15:53:56 -0400

    fixing up error in churn script

--- lib/scripts/comments_max_churn.rb
+++ lib/scripts/comments_max_churn.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 require 'csv'
 
-issue_com_hash = Hash.new(0)
+issue_comm_hash = Hash.new(0)
 issue_comm_hash = CodeReview.joins(patch_sets: [{patch_set_files: :comments}]).group(:issue).count('comments.id')
 
-CSV.open("/home/kd9205/chromium/history/data/churn_messages.csv", "wb") do |csv|
+CSV.open("/home/kd9205/chromium/history/data/churn_comments.csv", "wb") do |csv|
   CodeReview.find_each do |c|
     mc = c.max_churn
     comm_count = issue_comm_hash[c.issue]
