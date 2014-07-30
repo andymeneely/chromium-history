9a3b205db59ccb76097de72e0f14a6ec457a170f
Felivel Camilo <fdc7162@rit.edu>
2014-07-30 13:46:39 -0400

    Added folder creation to scrapers, and comments to the code.

--- lib/scripts/chrome_bugs_csv_scraper.rb
+++ lib/scripts/chrome_bugs_csv_scraper.rb
@@ -19,8 +19,6 @@ EOS
   opt :delay, "Set the amount of delay (in seconds) between get calls.", default: 0.25, type: Float
 end
 
-# This is a class for basic state storage and 
-# collection of Rietveld-scraping methods
 # 
 # @author Felivel Camilo
 class GoogleCodeBugScraperCSV  
@@ -29,10 +27,7 @@ class GoogleCodeBugScraperCSV
 
   @@file_location = './bugs/csv/'
   @@increment = 500
-  #@@baseurl = "http://code.google.com/feeds/issues/p/chromium/issues/full?alt=json&max-results=500&start-index="
-
- 
-  @@baseurl = "http://code.google.com/p/chromium/issues/csv?q=&colspec=Id+Summary+Blocked+BlockedOn+Blocking+Stars+Status+Reporter+Opened+Closed+Modified&can=1&num=500&start="
+  @@baseurl = "http://code.google.com/p/chromium/issues/csv?colspec=Id+Summary+Blocked+BlockedOn+Blocking+Stars+Status+Reporter+Opened+Closed+Modified&can=1&num=500&start="
 
 
   attr_accessor :ids, :data, :patches
@@ -44,42 +39,35 @@ class GoogleCodeBugScraperCSV
     @@baseurl
   end
 
-  # 
-  # Create a new instance
-  # @param  initial=nil Hash The initial values we have.
-  # This should match the output of to_hash
-  # 
-  # @return RietveldScraper Our new object
   def initialize(opts)
     @opts = opts
     @total = 0
-    @cursor = 1
+    @cursor = 1 
   end
 
-  # 
-  # Concurrently grabs corresponding data for each of the IDS we have.
-  # @param  with_messages=true Bool whether we want messages in the response
-  # 
-  # @return Array The data we've grabbed (a reference to our IVAR)
   def get_data(next_link="",delay=@opts[:delay], concurrent_connections=1)
-    #hydra = Typhoeus::Hydra.new(max_concurrency: concurrent_connections) # make a new concurrent run area
-
   
+    
+    
+    # Creates folder to store files.
+    FileUtils.mkdir_p(@@file_location) unless File.directory?(@@file_location)
+    
+    
+    
     puts "Fetching Data: #{@@baseurl+@cursor.to_s}"
-    issue_request = Typhoeus::Request.new(@@baseurl+@cursor.to_s)  # make a new request
+    issue_request = Typhoeus::Request.new(@@baseurl+@cursor.to_s)  
+    # make a new request
     
     issue_request.on_complete do |issue_resp|
       if issue_resp.success?
         
-        #bug_result = CSV.load(issue_resp.body) # push a Hash of the response onto our issues list
-        #puts bug_result
-        #CSV.to_file(@@file_location + "#{start_index}-#{@cursor-1}.json", bug_result)
-        
-        #extract the total number of records.
-        
         parsedData = CSV.parse(issue_resp.body)
         
+        #extract the total number of records.
         if @total == 0 
+          #reads the last line of the response file, 
+          #and extracts the total number of records.
+          
           @total =  parsedData[-1][0].scan(/\d+/)[1]
         end
         
--- lib/scripts/chrome_bugs_scraper.rb
+++ lib/scripts/chrome_bugs_scraper.rb
@@ -21,16 +21,12 @@ EOS
   opt :delay, "Set the amount of delay (in seconds) between get calls.", default: 0.25, type: Float
 end
 
-# This is a class for basic state storage and 
-# collection of Rietveld-scraping methods
-# 
-# @author Katherine Whitlock
-# @author Danielle Neuberger
+# @author Felivel Camilo
 class GoogleCodeBugScraper  
   # Set whether we want verbose debug output or not
   Typhoeus::Config.verbose = false 
 
-  @@file_location = './bugs/'
+  @@file_location = './bugs/json/'
   @@baseurl = "http://code.google.com/feeds/issues/p/chromium/issues/full?alt=json&max-results=500&start-index="
 
 
@@ -43,22 +39,16 @@ class GoogleCodeBugScraper
     @@baseurl
   end
 
-  # 
   # Create a new instance
   # @param  initial=nil Hash The initial values we have.
   # This should match the output of to_hash
   # 
-  # @return RietveldScraper Our new object
+  # @return GoogleCodeBugScraper  Our new object
   def initialize(opts)
     @opts = opts
     @cursor = 1
   end
 
-  # 
-  # Concurrently grabs corresponding data for each of the IDS we have.
-  # @param  with_messages=true Bool whether we want messages in the response
-  # 
-  # @return Array The data we've grabbed (a reference to our IVAR)
   def get_data(next_link="",delay=@opts[:delay], concurrent_connections=1)
        
     hydra = Typhoeus::Hydra.new(max_concurrency: concurrent_connections) # make a new concurrent run area
@@ -79,9 +69,12 @@ class GoogleCodeBugScraper
         if items_per_page > 0 #verifies if there is any items left      
           @cursor = start_index + items_per_page        
           
-          FileUtils.mkdir(@@file_location) unless File.directory?(@@file_location)                    
+
+          #Creates folder to store files.
+          FileUtils.mkdir_p(@@file_location) unless File.directory?(@@file_location)                    
           puts "#{start_index}- #{@cursor-1}: completed"
-          
+          puts "Processing replies..."
+
           bug_result["feed"]["entry"].each do |entry|
             entry_id = entry["issues$id"]["$t"]
             entry["link"].each do |link|
@@ -89,10 +82,9 @@ class GoogleCodeBugScraper
                 replies_request = Typhoeus::Request.new(link["href"]+"?alt=json&max-results=500")  # make a new request
                 replies_request.on_complete do |replies_resp|
                 if replies_resp.success?
-                    entry["replies"] = Oj.load(replies_resp.body)["feed"]["entry"]
-                    #FileUtils.mkdir(@@file_location + "replies") unless File.directory?(@@file_location + "replies")                    
-                    #File.open(@@file_location + "replies/#{entry_id}.json", "w") { |f| f.write(replies_resp.body) }
                     
+                    #embeds the replies in the original object.
+                    entry["replies"] = Oj.load(replies_resp.body)["feed"]["entry"]
                     puts "Replies for #{entry_id} completed"
                     sleep(delay)
                   end
