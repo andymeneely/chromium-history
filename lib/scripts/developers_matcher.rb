4fd140c275d6245dd810b07849f61bd586166a4d
Felivel Camilo <fdc7162@rit.edu>
2014-07-30 13:53:31 -0400

    More work on exploratory developer matcher script
    Currently broken since we took out de-ellipsization in sanitize_validate

--- lib/scripts/developers_matcher.rb
+++ lib/scripts/developers_matcher.rb
@@ -1,6 +1,13 @@
+require 'csv'
+require 'developer'
+
+
 class EmailMatcher
 
   def find_match(email)
+    
+    email.downcase!
+    
     #splits the string in name and domain.
     splited = email.split("...@")
 
@@ -9,20 +16,24 @@ class EmailMatcher
 
 
     #regular expression to find the matching email.
-    reg_exp = "^#{name}[^[:space:]]*@#{domain}"
+    #reg_exp = "^#{name}[^[:space:]]*@#{domain}"
+    reg_exp = "^#{name}[\\w]*@#{domain}"
     sql = "SELECT email FROM Developers WHERE email ~ '#{reg_exp}'"
-
+    
+    #disable active record logger.
+    ActiveRecord::Base.logger = nil
+    
     return Developer.find_by_sql(sql)    
   end
 end
 
 
-bug_emails = ['sa...@chromium.org',
+bug_emails = ['sa...@chormium.org',
               'p...@chromium.org',
-              'dar..@chromium.org',       
+              'dar..@google.org',       
               'b..@chromium.org',         
-              'm...@gmail.com',
-              'dea...@chromium.org',
+              'm...@google.com',
+              'simon%g...@gtempaccount.com',
               'ndu...@chromium.org',       
               'apa...@chromium.org',    
               'to...@chromium.org',       
@@ -31,7 +42,8 @@ bug_emails = ['sa...@chromium.org',
               'sk...@chromium.org',     
               'oj...@chromium.org',        
               'to...@chromium.org',   
-              'se...@chromium.org'
+              'se...@chromium.org',
+              'aa@google.com'
 ]
 
 
@@ -39,10 +51,11 @@ m = EmailMatcher.new
 
 
 bug_emails.each do |email|
+  puts Developer.sanitize_validate_email(email)
   matches = m.find_match(email)
     
   matches.each do |match|
-    puts "#{email} = #{match.email}"
+   # puts "#{email} = #{match.email}"
   end    
 end
 
