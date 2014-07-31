959d4a21612423efffd5a5ce49a5d02869762c56
Felivel Camilo <fdc7162@rit.edu>
2014-07-31 18:39:04 -0400

    Created joins for blocks and bloking bugs. Added join verifier.

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,5 +1,8 @@
 class Block < ActiveRecord::Base
   
+  belongs_to :blocked_on, primary_key: 'blocked_on_id', foreign_key: 'bug_id' 
+  belongs_to :blocking_bug, primary_key: 'blocking_id', foreign_key: 'bug_id'
+  
   def self.on_optimize
     ActiveRecord::Base.connection.add_index :blocks, :blocked_on_id 
     ActiveRecord::Base.connection.add_index :blocks, :blocking_id 
--- lib/verify/development/block_verify.rb
+++ lib/verify/development/block_verify.rb
@@ -10,4 +10,12 @@ class BlockVerify < VerifyBase
     assert_equal 17941, Block.find_by(blocking_id: 67890).blocked_on_id
   end
 
+  def verify_blocks_join_verify                                                                   
+    assert_equal "1", Bug.find(17941).blocks.exists?(:blocking_id => 67890)
+  end
+
+  def verify_blocking_join_verify                                           
+    assert_equal "1", Bug.find(17941).blocking.exists?(:blocked_on_id => 12345)
+  end
+
 end                                                                      
