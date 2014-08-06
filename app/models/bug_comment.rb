d47c667c34cb22aedfc5fb7846bb33b7631442eb
Andy Meneely <andy@se.rit.edu>
2014-08-05 20:23:16 -0400

    Another index we don't need

--- app/models/bug_comment.rb
+++ app/models/bug_comment.rb
@@ -3,7 +3,6 @@ class BugComment < ActiveRecord::Base
   
   def self.on_optimize
     ActiveRecord::Base.connection.add_index :bug_comments, :bug_id
-    ActiveRecord::Base.connection.add_index :bug_comments, :content
     ActiveRecord::Base.connection.add_index :bug_comments, :author_email
     ActiveRecord::Base.connection.add_index :bug_comments, :author_uri
     ActiveRecord::Base.connection.add_index :bug_comments, :updated
