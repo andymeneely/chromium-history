d8122f1d29f5f85835f1b8750d9d81779b5ee340
Felivel Camilo <fdc7162@rit.edu>
2014-08-12 11:52:56 -0400

    Modified bug portion of git loader for issue #176. Created Verify.

--- lib/loaders/git_log_loader.rb
+++ lib/loaders/git_log_loader.rb
@@ -240,12 +240,22 @@ class GitLogLoader
     bugs = bugs.split(%r{,\s*})
     
     bugs.each do |bug|
+      
+      #normalize bug field
+      bug.downcase!
       bug.strip!
-      #remove the word chormium if its exists.
-      bug.slice! "chromium:"
 
-      #if the bug is a number with 6 digits or less. 
+      #remove the common bad text
+      bad_text = ["chromium:","issue","bug=","="]
+      bad_text.each do |text|
+        bug.slice! text if bug.start_with? text
+      end
+
+      #if the bug is a number with 6 digits or if it is a repetition of 4-6 digits
       if bug.match(/^(\s*\d{1,6}\s*)$/) != nil
+        @con.exec_prepared('bugInsert', [commit_hash,bug.to_i])
+      elsif /([0-9]{4,6})\1/.match(bug) != nil
+        bug = /([0-9]{4,6})\1/.match(bug)[1]
         @con.exec_prepared('bugInsert', [commit_hash,bug.to_i])
       end
     end
--- lib/verify/development/commit_association_verify.rb
+++ lib/verify/development/commit_association_verify.rb
@@ -4,12 +4,14 @@ class CommitAssociationVerify < VerifyBase
   def verify_reviewers_for_6eebdee
     assert_equal 3, Commit.where(commit_hash: '6eebdee7851c52b1f481fca1cdffcbc51c8ec061').take.reviewers.count
     assert_equal ['danakj@chromium.org','ojan@chromium.org','skaslev@chromium.org'], Commit.where(commit_hash: '6eebdee7851c52b1f481fca1cdffcbc51c8ec061').take.reviewers.pluck(:email).to_a.sort
-
   end
 
   def verify_commit_filepath_to_cve
     assert_equal true, CommitFilepath.where(filepath: 'ui/base/x/x11_util.cc').take.cve?
   end
 
+  def verify_bugs_for_1bcf224
+    assert_equal 229611, Commit.joins(:commit_bugs).where(:commit_hash => '1bcf224fd40b497997827af5f04b0628e648700f').pluck(:bug_id)[0]
+  end
 end
 
