b7282304ec6323358c8ebb0e7bc2b795dda1c3a9
Andy Meneely <andy@se.rit.edu>
2014-09-10 11:08:04 -0400

    Cleanup for collection

--- lib/scripts/release-filepaths.sh
+++ lib/scripts/release-filepaths.sh
@@ -2,13 +2,15 @@
 
 # We're defining Jan 28 2011 as our release date
 # Manual inspection of that git log makes the official commit as being 
-# COMMIT=c1bf8951e07a42ecc493ed2e4481211dabbc5f61
-# RELEASE=11.0
+#
+COMMIT=c1bf8951e07a42ecc493ed2e4481211dabbc5f61
+RELEASE=11.0
 
 # For a second release, we're using Feb 2 2012 as another release date of 19.0
 # Manual inspection of git makes that official commit as being
-COMMIT=d653361472c0f286b260f68d5032173d505661b1
-RELEASE=19.0
+#
+#COMMIT=d653361472c0f286b260f68d5032173d505661b1
+#RELEASE=19.0
 
 # Use git ls-tree and then just take the filename part of the output with awk
 # Note that Release v11 is hardcoded into that print command
