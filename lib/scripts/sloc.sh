5f5c9f744a756449f06441de2373fd1ecb67a8fd
Kayla Davis <kd9205@rit.edu>
2014-09-25 17:37:55 -0400

    sloc.sh script now names the file from params
    documentation for properly running the script added

--- lib/scripts/sloc.sh
+++ lib/scripts/sloc.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# When you run this in chromium/src make sure to run in the following way:
+# ../history/lib/scripts/sloc.sh X.X
+# where X.X is the release number.
+# EX:  ../history/lib/scripts/sloc.sh 11.0
+#
+# This names the file created from this script X.X.csv
+# In the example the output file would be named 11.0.csv
+
 # outputs language,filename,blank,comment,code
-# into a csv
 #
 # --skip-uniqueness because some files are repeated but with different paths
 # (but then we have to sort and uniq them to remove duplicate lines)
 #
 # Chop off the top two header rows too (three newlines)
-cloc --by-file --force-lang="Lisp",sb --force-lang="Python",gyp --force-lang="Python",scons --progress-rate=0 --quiet --csv --skip-uniqueness . | tail -n +3 - | sort | uniq > sloc.csv
-
+cloc --by-file --force-lang="Lisp",sb --force-lang="Python",gyp --force-lang="Python",scons --progress-rate=0 --quiet --csv --skip-uniqueness . | tail -n +3 - | sort | uniq > $1.csv
