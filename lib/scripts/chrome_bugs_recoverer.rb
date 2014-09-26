2fd40c90fc2eae898bf456c23d4374d8cc774675
Felivel Camilo <fdc7162@rit.edu>
2014-09-25 20:17:37 -0400

    add file splits at 500 records for json bug recoverer.

--- lib/scripts/chrome_bugs_recoverer.rb
+++ lib/scripts/chrome_bugs_recoverer.rb
@@ -14,6 +14,8 @@ class GoogleCodeBugRecoverer
 
   def initialize()
     @bugs = Array.new
+    @file_index = 1
+    @file_size = 500
   end
 
 
@@ -27,11 +29,15 @@ class GoogleCodeBugRecoverer
     + "FROM #{arg[:many_table]} RIGHT OUTER JOIN #{arg[:one_table]} " \
     + "ON (#{arg[:many_table]}.#{arg[:many_table_key]} = #{arg[:one_table]}.#{arg[:one_table_key]}) " \
     + "WHERE #{arg[:many_table]}.#{arg[:many_table_key]} IS NULL"
-   
     
     st = ActiveRecord::Base.connection.execute query
     st.each do |row|
+
       recover_bug(row["bug_id"])
+      if @bugs.size >= @file_size
+        puts @bugs.size
+        save_bugs()
+      end
     end
   end
 
@@ -40,34 +46,41 @@ class GoogleCodeBugRecoverer
     FileUtils.mkdir_p(@@file_location) unless File.directory?(@@file_location)
     issue_request = Typhoeus::Request.new(@@baseurl+bug_id.to_s)  # make a new request
     issue_request.on_complete do |issue_resp|
-      bug_result = Oj.load(issue_resp.body)
-      bug_result["feed"]["entry"].each do |entry|
-        entry_id = entry["issues$id"]["$t"]
-        puts "Entry #{entry_id} completed"
-        entry["link"].each do |link|
-          if link["rel"] == "replies"
-            replies_request = Typhoeus::Request.new(link["href"]+"?alt=json&max-results=500")  # make a new request
-            replies_request.on_complete do |replies_resp|
-              if replies_resp.success?   #embeds the replies in the original object.
-                entry["replies"] = Oj.load(replies_resp.body)["feed"]["entry"]
-                puts "Replies for #{entry_id} completed"
-                sleep(0.25)
+      #if issue_resp.code = 200
+      if issue_resp.success?
+        bug_result = Oj.load(issue_resp.body)
+        bug_result["feed"]["entry"].each do |entry|
+          entry_id = entry["issues$id"]["$t"]
+          puts "Entry #{entry_id} completed"
+          entry["link"].each do |link|
+            if link["rel"] == "replies"
+              replies_request = Typhoeus::Request.new(link["href"]+"?alt=json&max-results=500")  # make a new request
+              replies_request.on_complete do |replies_resp|
+                if replies_resp.success?   #embeds the replies in the original object.
+                  entry["replies"] = Oj.load(replies_resp.body)["feed"]["entry"]
+                  puts "Replies for #{entry_id} completed"
+                  sleep(0.25)
+                end
               end
+              replies_request.run
             end
-            replies_request.run
-          end
-        end 
-      @bugs << entry
+          end 
+        @bugs << entry
+        end
       end
     end
     issue_request.run
   end
 
   def save_bugs()
-    Oj.to_file(@@file_location + "recovered.json", @bugs)
+    Oj.to_file(@@file_location + "recovered_#{@file_index}.json", @bugs)
+    puts "File recovered_#{@file_index}.json Saved"
+
+    @bugs = Array.new
+    @file_index += 1
   end
 end
 
 r = GoogleCodeBugRecoverer.new()
 r.recover_dangling()
-r.save_bugs
+r.save_bugs()
