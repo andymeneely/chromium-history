2f95495a171e804a095a528f9165244410ea1457
Richard Kalimba Iraguha <rik1209@rit.edu>
2014-10-03 13:19:53 -0400

    refactored solution to #193 using case statements instead of if/else added some methods for routine tasks (adding files, getting relative paths, etc..) using regular expressions to match ownership rules

--- lib/scripts/parse_release-filepaths-OWNERS.rb
+++ lib/scripts/parse_release-filepaths-OWNERS.rb
@@ -45,126 +45,126 @@ class ParseReleaseFilepathsOwners
     @srcLoc = srcLoc
     getOwnerShip(Array.new, srcLoc)
   end
-
-  def getOwnerShip(pOwners, currDir)
-    #change to currDir
-    Dir.chdir currDir
-    currOwners = Array.new
-    cfile = CSV.open(@csvLoc, "a")
-
-    #get immediate files in current directory
-    allFiles = Dir.glob("*.*")
-
-    #get relative paths if not baseDir
-    if (allFiles.respond_to?("each") and !(Dir.pwd.to_s.eql?(@srcLoc)))
-      relative = Pathname.new(Dir.pwd).relative_path_from Pathname.new(@srcLoc)
-      allFiles.map!  {|filename| filename = relative.to_s + "/" + filename}
-    end
-
-    #get the new owners: currOwners from OWNERS file in current directory if there is one : currDir
-    if File.file?("OWNERS.txt")
-      f = File.open("OWNERS.txt")
-
-      #Process the owners file
-      #if it has set noparent as first rule in file, drop pOwners. currOwners only are to own sub directories. if all are owners record that too
-      rule = f.readline
-      if rule.eql?("set noparent\n")
-        pOwners = Array.new
-        rule = f.readline
-      elsif rule.eql?("*\n")
-        currOwners << "ALL"
-        rule = f.readline
-      end
-      rule = rule.sub("\n", "")
-
-      #add all emails at beginning to currOwners until per-file rule is met.
-      until (rule.include?("per-file")  or f.eof?) do
-        currOwners << rule
-        rule = f.readline
-        #if comment or blank, get next
-        while (rule.slice(0).eql?("#") or rule.eql?("\n"))
-          rule = f.readline
-        end
-        rule = rule.sub("\n", "")
-      end
-
-      #send currOwners to all immediate sub-directories, so their files know who are their parents, get all childFiles in return
-      childFiles = Array.new
-      if Dir.glob("*/").respond_to?("each")
-        Dir.glob("*/").each {|nxtDir| childFiles += getOwnerShip(pOwners+currOwners,nxtDir )}
-        #will store childFiles returned to be included for per-file rules.
-      end
-
-      #all files under this directory
-      allFiles += childFiles
-
-      noPFiles = Array.new  #for files that have no parent
-
-      #process per-file rules
-      until (!(rule.include?("per-file")) or f.eof?) do
-        fileRule  = rule
-
-        #if per-file rule has set noparent, then get the globbed files to a new list of files without parents
-        if rule.include?("set noparent")
-    	  glob = (rule.sub("=set noparent", "")).sub("per-file ","")
-
-	  allFiles.each do |filename|
-            if File.fnmatch(glob, filename)
-	      noPFiles << filename
-	      allFiles.delete(filename)
-	    end
-	  end
-
-        else
-	  #if per-file rule has email of owner, add to csv all files from allFiles or noPFiles matching glob
-	  glob = (rule.split("=")[0]).sub("per-file ","")
-	  email = rule.sub("per-file "+glob+"=","")
-	  if email.eql?("*")
-	    email = "ALL"
-	  end
-	  #add each version, filename, owner to csv
-	  (allFiles+noPFiles).each do |filename|
-	    if File.fnmatch(glob, filename)
-	      cfile << ["11","#{filename}", "#{email}"]
-	    end
-	  end
-
-        end
-
-        rule = f.readline
-        #if comment or blank, get next
-        while (rule.slice(0).eql?("#") or rule.eql?("\n"))
-	  rule = f.readline
-        end
-        rule = rule.sub("\n", "")
-
-      end
-
-      #add each version, filename, owner to csv for global owners
-      allFiles.each do |filename|
-        (currOwners+pOwners).each do |owner|
+  
+  def getRelativePath (filename)
+	relative = Pathname.new(Dir.pwd).relative_path_from Pathname.new(@srcLoc)
+	filename = relative.to_s + "/" + filename
+    return filename
+  end
+  
+  def setFilesWithNoParents(allFiles, noPFiles, glob)
+	allFiles.each do |filename|
+			if File.fnmatch(glob, filename)
+				noPFiles << filename
+				allFiles.delete(filename)
+			end
+		end
+  end
+  
+  def addFileOwnersToCsv(files, owners)
+  CSV.open(@csvLoc, "a") do |cfile|
+  files.each do |filename|
+        owners.each do |owner|
            cfile << ["11","#{filename}", "#{owner}"]
         end
       end
-
-      f.close
-    end
-
-    cfile.close
-    Dir.chdir("..")
-
-    if (pOwners.empty?)
-      return Array.new
-    else
-      return allFiles 
-    end
-
+	  end
+  end
+  
+  def addSingleFileOwnerToCsv(files, owner, glob)
+  CSV.open(@csvLoc, "a") do |cfile|
+  #add each version, filename, owner to csv
+	  (files).each do |filename|
+	    if File.fnmatch(glob, filename)
+	      cfile << ["11","#{filename}", "#{owner}"]
+	    end
+	  end
+	  end
   end
 
-end
-
+  def getOwnerShip(pOwners, currDir)
+    
+	allFiles = Array.new
+	
+    Dir.chdir currDir do
+	
+    #get immediate files in current directory, get relative paths if not baseDir
+    allFiles = Dir.glob("*.*")
+	(allFiles.map! {|f| getRelativePath(f)}) if !(Dir.pwd.to_s.eql?(@srcLoc))
+	
+	currOwners = Array.new
+	rules = Array.new
+	
+	#get the new owners: currOwners from OWNERS file in current directory if there is one : currDir
+	rules = File.open("OWNERS").readlines if File.file?("OWNERS")
+	index = -1
+	
+	rules.each_with_index do |rule, i|
+	index = i
+	case rule
+	
+	when  /^set noparent/
+	pOwners = Array.new
+	
+	when /^\*$/	#rule.eql?("*\n")
+	currOwners << "ALL"
+	
+	when /^[^=][a-z0-9_-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+	rule.slice!(-1)
+	currOwners << rule
+	
+	when rule.slice(0).eql?("#") , rule.eql?("\n")
+	when /per-file.+/
+	break
+	
+	end
+	
+	end
+	
+	#send currOwners to all immediate sub-directories, so their files know who are their parents, get all childFiles in return
+    childFiles = Array.new
+    Dir.glob("*/").each {|nxtDir| childFiles += getOwnerShip(pOwners+currOwners,nxtDir )}
+	
+	#all files under this directory
+    allFiles += childFiles
+	noPFiles = Array.new  #for files that have no parent
+	
+	rules.drop(index).each do |rule|
+	case rule
+	when rule.slice(0).eql?("#") , rule.eql?("\n")
+	#do nothing
+	when /per-file.+=.*\*/
+	glob = (rule.split("=")[0]).sub("per-file ","")
+	email = "ALL"
+	addSingleFileOwnerToCsv(allFiles+noPFiles, email, glob)
+	
+	when /per-file.*set noparent/
+	glob = (rule.sub("=set noparent", "")).sub("per-file ","")
+	setFilesWithNoParents(allFiles, noPFiles, glob)
+	
+	when /per-file.+@.+/
+	rule.slice!(-1)
+	glob = (rule.split("=")[0]).sub("per-file ","")
+	email = rule.sub("per-file "+glob+"=","")
+	addSingleFileOwnerToCsv(allFiles+noPFiles, email, glob)
+	
+	end
+	end unless index == -1 || index+1 >= rules.length
+	
+	#add each version, filename, owner to csv for global owners
+	addFileOwnersToCsv(allFiles, currOwners+pOwners )
+	
+     
+	end
+	
+	return (pOwners.empty?)? Array.new : allFiles
+	
+	end
+	end
+	
 #driver code
 p = ParseReleaseFilepathsOwners.new(opts)
 p.getOwnerShipHelper
 
 puts "Done."
+
