346a050c485638e36c7d79424131784cce33ed9f
Richard Kalimba Iraguha <rik1209@rit.edu>
2014-10-03 13:23:10 -0400

    sample test directory to parse owners #193

---
+++ lib/scripts/owners_test/src/cc/log/child_process_security_policy_impl.h
@@ -0,0 +1,19 @@
+sky@chromium.org
+
+per-file child_process_security_policy_impl.*=tsepez@chromium.org
+per-file child_process_security_policy_impl.*=creis@chromium.org
+per-file child_process_security_unittest.cc=tsepez@chromium.org
+per-file child_process_security_unittest.cc=creis@chromium.org
+per-file power_save_blocker_chromeos.cc=derat@chromium.org
+
+# Mac Sandbox profiles.
+per-file *.sb=set noparent
+per-file *.sb=jeremy@chromium.org
+
+# Changes to this file may break telemetry benchmarks
+per-file benchmark_instrumentation.h=set noparent
+per-file benchmark_instrumentation.h=ernstm@chromium.org
+per-file benchmark_instrumentation.h=nduca@chromium.org
+per-file benchmark_instrumentation.cc=set noparent
+per-file benchmark_instrumentation.cc=ernstm@chromium.org
+per-file benchmark_instrumentation.cc=nduca@chromium.org
