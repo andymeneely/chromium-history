89f844fbb772130977141f132bdd47c9c655099d
Danielle Neuberger <drn5447@archeology.gccis.rit.edu>
2014-11-11 17:11:09 -0500

    Added Trollop, refining other parts of script. Still issues with loading developer.rb for email parsing, and errors checking out commits (potentially unrelated to script).

--- lib/scripts/first-ownership-script.rb
+++ lib/scripts/first-ownership-script.rb
@@ -1,31 +1,49 @@
 require "csv"
+require "trollop"
+
+
+# Trollop Command-line parameters
+opts = Trollop::options do
+  version "First OWNERShip script"
+  banner <<-EOS
+
+The first ownership script takes in a text file of commit hashes (separated by new lines) for commits that have modified any OWNERS file. 
+
+Produces a CSV including developer email, directory of OWNERS file, and commit hash for when the developer was first added to the OWNERS file.
+
+Usage: 
+  ruby #{File.basename(__FILE__)} [options]
+
+where [options] are:
+  EOS
+  opt :commitsFile, "The file name including path of the file containing the commit hashes to use", :required => true, type: String
+  opt :csv, "The output CSV file for the results", default: "first-owners.csv", type: String
+end 
+
 
 #
 # A scraper to find the time a person was initially added as an OWNER
 # 
 # @author Danielle Neuberger
 class FirstOwnershipScript
-  # Class-level variables
   @@hashmap # contains hash of owners information 
   @@commitNumsFile # the file name for the commitHashs modifying OWNERS files
   @csvLoc # the location of the CSV file to produce
 
   # Create a new isntance
   # @return FirstOwnershipScript instance - the new object
-  def initialize()
-    # Assume first commandline arg is the file output of commit hashes that contain
-    # modifications to any OWNERS file TODO change this to use Trollop or something??
-    @@commitNumsFile = ARGV[0] #TODO would need to throw error if this isnt present
-    @csvLoc = "../Results.csv"
+  def initialize(opts)
+    File.open(opts[:csv], 'w+').close
+    @@commitNumsFile = File.expand_path(opts[:commitsFile])
+    @csvLoc = File.expand_path(opts[:csv])
   end
 
   # Method to loop through commit nums in the passed file and call helper methods
   # to pull information for each 
-  def get_ownership()
-    commitHashes = File.read(@@commitNumsFile).split(",").map(&:strip) #TODO test
-    commitHashes.each do |hash| 
-      analyze_commit_file(hash)
-    end    
+  def get_ownership()    
+    File.readlines(@@commitNumsFile).each do |line|
+      analyze_commit_file(line) #assumes each line has only the commit hash
+    end
     create_csv()
   end
 
@@ -33,23 +51,25 @@ class FirstOwnershipScript
   # owners emails and puts the information along with the commit hash
   # , path, and date into the owner-ownerfile hash
   def analyze_commit_file(commitNum)
-    files = `git checkout #{commitNum}`
-    date = `git show -s --format%ci #{commitNum}` #TODO %cd vs %ci ?
-    files.each do |file|
-      puts file #see what the file name is - how do we get the directory?
-      if file=="OWNERS" #if the file is an owners file
-        get_owners_info(file, commitNum, date) #TODO might need to get file dir for here
+    puts commitNum
+    `git checkout #{commitNum}`
+    filesWithPaths = `git diff-tree --no-commit-id --name-only -r #{commitNum}`
+    puts "Files for commit :" + filesWithPaths #TODO remove later, only for debugging
+    date = `git show -s --format=%ci #{commitNum}`
+    filesWithPaths.each do |file|
+      if file.include? "OWNERS"  #if the file is an owners file
+	path = File.dirname(file) + "/"
+        get_owners_info(file, commitNum, date, path)
       end
     end
   end
 
   # Open up an owners file and add information to the hash
-  def get_owners_info(file, commitNum, date)
-    File.foreach(file) do |line|
-      if contains_email(line)
-	email = line.match(/^[^=][a-z0-9_-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) #TODO test this regex
-	directory = "" #TODO get this
- 	key = email + "~" + directory
+  def get_owners_info(file, commitNum, date, path)
+    File.open(file) do |file|
+      file.each do |line|
+        email = sanitize_validate_email(line) #TODO load Kayla's script first and use this method from there
+ 	key = email + "~" + path
 	value = [commitNum, date]
         if @@hashmap.key?(key)
 	  currHashDate = @@hashmap[key][0]
@@ -77,7 +97,7 @@ end
 
 
 #driver code
-f = FirstOwnershipScript.new()
+f = FirstOwnershipScript.new(opts)
 f.get_ownership()
 
 puts "Done."
