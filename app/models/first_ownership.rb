1d986341287fe27dc25df743599b0d5d68b87eab
Richard Kalimba Iraguha <rik1209@rit.edu>
2014-12-09 20:01:17 -0500

    Changes to model for consistency New first owners csv

--- app/models/first_ownership.rb
+++ app/models/first_ownership.rb
@@ -1,12 +1,11 @@
 class FirstOwnership < ActiveRecord::Base
   belongs_to :releaseowner, foreign_key: 'owner_email', primary_key: 'owner_email'
   belongs_to :releaseowner, foreign_key: 'dev_id', primary_key: 'dev_id'
-  belongs_to :filepath, foreign_key: 'filepath', primary_key: 'filepath'
 
   def self.optimize
-	connection.add_index :first_ownerships, :filepath
+	connection.add_index :first_ownerships, :directory
     connection.add_index :first_ownerships, :owner_email
-    connection.add_index :first_ownerships, [:filepath,:owner_email]
-    connection.execute "CLUSTER first_ownerships USING index_first_ownerships_on_filepath_and_owner_email"
+    connection.add_index :first_ownerships, [:directory,:owner_email]
+    connection.execute "CLUSTER first_ownerships USING index_first_ownerships_on_directory_and_owner_email"
   end
 end
