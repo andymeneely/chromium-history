5890bb7b94adbf3c5e783646ac2a2855650d654a
bspates <bxs4361@g.rit.edu>
2015-01-26 17:39:23 -0500

    fix block declaration and use

--- lib/loaders/vocab_loader.rb
+++ lib/loaders/vocab_loader.rb
@@ -18,24 +18,24 @@ class VocabLoader
     Message.get_all_messages raw
     VocabLoader.clean_file raw, @tmp_dir+'/messages.txt'
     File.unlink raw
-    acm_corpus = Corpus.new "#{@data_dir}/raw_acm.txt"
+    acm_corpus = Corpus.new "#{@data_dir}/acm/raw_acm.txt"
     comment_corpus = Corpus.new "#{@tmp_dir}/comments.txt"
     message_corpus = Corpus.new "#{@tmp_dir}/messages.txt"
     comment_corpus.filter
     message_corpus.filter
     words = comment_corpus.word_intersect acm_corpus.words
     words = words + message_corpus.word_intersect(acm_corpus.words)
-    block do |table|
+    block = lambda do |table|
       words.each do |word|
         table << [word]
       end
     end
-    copy_results "#{@tmp_dir}/vocab.csv", 'technical_words', block
+    copy_results "#{@tmp_dir}/vocab.csv", 'technical_words', &block
     ActiveRecord::Base.connection.execute "ALTER TABLE technical_words ADD COLUMN id SERIAL; ALTER TABLE technical_words ADD PRIMARY KEY (id);"
   end
 
   def associate_developer_vocab
-    block do |table|
+    block = lambda do |table|
       convos = Comment.get_developer_comments
       table = reassociate convos, 'author_id', 'string_agg', table
       convos.clear
@@ -43,19 +43,19 @@ class VocabLoader
       table = reassociate messages, 'sender_id', 'string_agg', table
       messages.clear
     end
-    copy_results "#{@tmp_dir}/dev_words.csv", 'developers_technical_words', block
+    copy_results "#{@tmp_dir}/dev_words.csv", 'developers_technical_words', &block
   end
 
   def associate_code_review_vocab
-    block do |table|
+    block = lambda do |table|
       convos = Comment.get_all_convo
       table = reassociate convos, 'code_review_id', 'string_agg', table
       convos.clear
       messages = Message.get_all_messages
-      table = reassociate convos, 'code_review_id', 'string_agg', table
+      table = reassociate messages, 'code_review_id', 'string_agg', table
       messages.clear
     end
-    copy_results "#{@tmp_dir}/code_review_words.csv", 'code_reviews_technical_words', block
+    copy_results "#{@tmp_dir}/code_review_words.csv", 'code_reviews_technical_words', &block
   end
 
   def copy_results file_name, table_name, &block
@@ -67,7 +67,7 @@ class VocabLoader
 
   def reassociate origins, origin_id_key, origin_text_key, table
     origins.each do |origin|
-      clean = VocablLoader.clean origin[origin_text_key]
+      clean = VocabLoader.clean origin[origin_text_key]
       results = @search_tree.search clean
       results.each do |result|
         table << [origin[origin_id_key], result.id]
