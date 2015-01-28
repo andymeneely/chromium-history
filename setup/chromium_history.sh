23b611020cc3b70d3a3e75a4cc15dd90676db11a
bspates <bxs4361@g.rit.edu>
2015-01-28 11:03:43 -0500

    add tmp folder creation and permission changes to profile.d for development

--- setup/chromium_history.sh
+++ setup/chromium_history.sh
@@ -2,3 +2,5 @@ export RAILS_ENV="development"
 export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"
 export STANFORD_NLP="/opt/stanford-core-nlp/"
 export PUNKT_MODELS="/opt/punkt/"
+mkdir -p /tmp/vagrant/development
+chmod 777 /tmp/vagrant/development
