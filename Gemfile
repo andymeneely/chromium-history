d91e22dae5a357c7f2dc535807c0b23f1c10f0fc
bspates <bxs4361@g.rit.edu>
2015-02-01 12:05:29 -0500

    added oauth type indentification

--- Gemfile
+++ Gemfile
@@ -63,10 +63,9 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'nokogiri'
 
-# For downloading our GoogleSheets data
-# As of Jun 18 2014 this is still not release for the google_drive gem, switching temporarily to master to get it working with sheets
-#gem 'google_drive', '>= 0.3.9' 
-gem 'google_drive', git: 'https://github.com/gimite/google-drive-ruby.git'
+gem 'google_drive', '~> 1.0.0'
+
+gem 'google-api-client'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- lib/loaders/cve_loader.rb
+++ lib/loaders/cve_loader.rb
@@ -2,12 +2,13 @@
 
 require "csv"
 require "google_drive"
+require "google/api_client"
 
 class CveLoader
   RESULTS_FILE = "#{Rails.configuration.datadir}/cves/cves.csv"
 
   def load_cve
-    download_csv unless Rails.env == 'development' 
+    download_csv #unless Rails.env == 'development' 
     parse_cves
     copy_to_db
   end
@@ -16,7 +17,20 @@ class CveLoader
   # username and password specified in credentials.yml
   def login
     google_creds_yml = YAML.load_file("#{Rails.root}/config/credentials.yml")['google-docs']
-    GoogleDrive.login(google_creds_yml['user-name'], google_creds_yml['password'])
+    client = Google::APIClient.new
+    auth = client.authorization
+    auth.client_id = google_creds_yml['client-id']
+    auth.client_secret = google_creds_yml['client-secret']
+    auth.scope =
+        "https://www.googleapis.com/auth/drive " +
+        "https://spreadsheets.google.com/feeds/"
+    auth.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'#google_creds_yml['redirect_uri']
+    print("1. Open this page:\n%s\n\n" % auth.authorization_uri)
+    print("2. Enter the authorization code shown in the page: ")
+    auth.code = $stdin.gets.chomp
+    auth.fetch_access_token!
+    access_token = auth.access_token
+    GoogleDrive.login_with_oauth(access_token)
   end
 
   #Go out to Google Drive and download the sheet
@@ -24,7 +38,8 @@ class CveLoader
     File.delete(RESULTS_FILE) if File.exists?(RESULTS_FILE)
     session = login()
     spreadsheet = session.spreadsheet_by_key(Rails.configuration.google_spreadsheets['key'])
-    spreadsheet.export_as_file(RESULTS_FILE, 'csv', Rails.configuration.google_spreadsheets['gid'])
+    worksheet = spreadsheet.worksheet_by_title 'CVEs'
+    worksheet.export_as_file(RESULTS_FILE, 'csv', Rails.configuration.google_spreadsheets['gid'])
   end
 
   def parse_cves
