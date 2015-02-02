44da588c3df9c3d1023d9dad3a13e8de7bb136ae
vagrant <vagrant@precise64-basebox.(none)>
2015-02-02 18:48:31 +0000

    Updated the ajacency query call.

--- lib/analysis/participant_analysis.rb
+++ lib/analysis/participant_analysis.rb
@@ -1,22 +1,25 @@
 class ParticipantAnalysis 
 
-  # At a given code review, each participant may have had prior experience with the code
-  # review's owner. Count those prior experiences and update reviews_with_owner 
+  # At a given code review, each participant may have had prior experience with
+  # the code review's owner. Count those prior experiences and update 
+  # reviews_with_owner 
   def populate_reviews_with_owner
     Participant.find_each do |participant|
-      c = participant.code_review
+      cr = participant.code_review
 
-      #find all the code reviews where the owner is owner and one of the reviewers is participant
-      #and only include reviews that were done before this one
+      # Find the count of all code reviews where the owner is owner and one of
+      # the reviewers is participant and only include reviews that were done
+      # before this one.
       reviews = Participant\
-        .where("owner_id = ? AND review_date < ? AND dev_id = ? AND dev_id<>owner_id ", \
-               c.owner_id, c.created, participant.dev_id)
+        .where("owner_id = ? AND review_date < ? AND dev_id = ? AND dev_id <> owner_id ", \
+        cr.owner_id, cr.created, participant.dev_id)
 
       participant.update(reviews_with_owner: reviews.count)
     end
   end#method
 
-  # At the given code review, each participant may or may not have had experience in bug-label related reviews. 
+  # At the given code review, each participant may or may not have had 
+  # experience in bug-label related reviews. 
   def popbugulate_bug_related_experience
     update=<<-eos
     UPDATE participants 
@@ -29,49 +32,58 @@ class ParticipantAnalysis
     FROM developers WHERE developers.id = participants.dev_id;
     eos
     ActiveRecord::Base.connection.execute update
-  end
+  end#method
 
   # At the given code review, total the number of sheriff hours that the participant has had
   def populate_sheriff_hours
     Participant.find_each do |participant|
       date = participant.code_review.created
-      sheriff_hours = SheriffRotation.where("start < ? AND dev_id = ?", date, participant.dev_id).pluck(:duration)
+      sheriff_hours = SheriffRotation.where("start < ? AND dev_id = ?", date, \
+        participant.dev_id).pluck(:duration)
 
       participant.update(sheriff_hours: sheriff_hours.inject(0, :+))
     end
-  end
+  end#method
 
-  #
+  # Determine the security experienced participants (SEP) who a given
+  # participant has worked with before a given code review.
+  # The assumption is made of the knowledge transfer from a SEP to a non-SEP (NSEP).
   def populate_security_adjacencys
-    # all code reviews that have cvenums (looking at vulnerabilities) and all the participants in it. 
-    # total_participants = cvenum.joins(:code_reviews).joins(:participants)
+    # Get all participants from security related code reviews
+    all_participants = Participant.joins(code_reviews: :cvenums)
+
+    # In issue #184, the examples indicate that participants will gain security
+    # adjacency's through non-security related code reviews.
+    # With the aforementioned assumption, it seems like it would be more easily
+    # accepted that a NSEP would have a greater knowledge transfer in security
+    # code reviews then not. We may want to see both and see if the correlation
+    # is higher for one or the other.
+    # For just all code reviews, we'd want the line below.
+    # all_Participants = Participant.joins(:code_reviews)
 
     Participant.find_each do |participant|
-      #all the security experienced participants that are not the given participant. Used with group, it returns a hash whoses keys represent dev_id and the value are the count
+    # The code review the participant is in.
+      cr = participant.code_review
 
-      #I believe this way is the more appropriate way. 
-      # s_participants = total_participants.group(:dev_id).count(:dev_id, :conditions => ["security_experienced = true AND dev_id <> ?", participant.dev_id])
+      # Get all SEP counts from all prior code reviews a given participant was in.
+      sep_adj = all_participants.count(:dev_id, :conditions => \
+        ["security_experienced = true AND dev_id <> ? AND review_date < ?", \
+        participant.dev_id], cr.created).distinct
 
-      #This could also be a possibility. And maybe the :group needs to go before the :condition
-       # s_participants = total_participants.count(:dev_id, :conditions => ["security_experienced = true AND dev_id <> ?", participant.dev_id], :group => "dev_id")
-      # from here I need to put these dev_ids and the counts into the adjacency table
+      # We don' need to group if we just want distinct, because it will be
+      # grouping puts it into a hash, then getting the count of a hash gives you
+      # how many times that group col showed up.
+      # I think we could do .group(:dev_id). \ 
+        # having("security_experienced = true AND dev_id <> ? AND review_date < ?", \
+        # participant.dev_id], cr.created").size
 
-      # code review the participant is in
-      c = participant.code_review
+      # Get all SEP counts from all prior code reviews a given participant was in.
+      # sep_adj = all_participants.group(:dev_id).count(:dev_id, :conditions => \
+        # ["security_experienced = true AND dev_id <> ? AND review_date < ?", \
+        # participant.dev_id], cr.created).distinct
 
-      if c.is_inspecting_vulnerability?
-        # i'm unsure which of the following two are correct yet
-        # only security experienced participants and not the given one
-        s_adjacencys_count = c.participants.group(:dev_id).count(:dev_id, :conditions => ["security_experienced = true AND dev_id <> ?", participant.dev_id])
-
-        # the :group may need to come before :conditions to be symmantically equivalent to the statement above
-        # s_adjacencys_count = c.participants.count(:dev_id, :conditions => ["security_experienced = true AND dev_id <> ?", participant.dev_id], :group => "dev_id")
-
-        participant.security_adjacencys = s_adjacencys_count
-
-        participant.save
-      end
+      participant.security_adjacencys = sep_adj
+      participant.save
     end
-  end
-
+  end#method
 end#class
