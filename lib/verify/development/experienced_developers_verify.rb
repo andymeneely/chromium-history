991d2b9c1081393a4735bec8628b0b350124a16c
Andy Meneely <andy@se.rit.edu>
2015-02-09 11:53:57 -0500

    Injected a new participant for verify purposes

--- lib/verify/development/experienced_developers_verify.rb
+++ lib/verify/development/experienced_developers_verify.rb
@@ -3,7 +3,8 @@ require_relative "../verify_base"
 class ExperiencedDevelopersVerify < VerifyBase
 
   def verify_participants_security_adjacencys
-    assert_equal [1], Developer.find(6).participants.where('issue = 5831706594508800').pluck('security_adjacencys')
+    dev = Developer.find_by(email: 'senorblanco@chromium.org')
+    assert_equal [1], dev.participants.where('issue = 5831706594508800').pluck('security_adjacencys')
   end
 
 end#class
