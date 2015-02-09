86d0d97cdeaa0a78189d384450128e024f0b018d
Richard Kalimba Iraguha <rik1209@rit.edu>
2015-02-09 13:46:03 -0500

    modified first ownership loader now only updates release owner

--- lib/loaders/first_ownership_loader.rb
+++ lib/loaders/first_ownership_loader.rb
@@ -1,44 +1,18 @@
 require 'csv'
-require	"pathname"
 
 class FirstOwnershipLoader
 
   def load
     datadir = File.expand_path(Rails.configuration.datadir)
-    tmp = Rails.configuration.tmpdir
-    ownership = CSV.open("#{tmp}/first-ownership.csv", 'w+')
-
+	
     CSV.foreach("#{datadir}/first_ownership/first-owners.csv") do |line|
 	  em = Developer.search_or_add(line[0]) 
 	  unless(em[0].nil?)
-	    if(ReleaseOwner.exists?(:owner_email => em[0], :directory => line[1]))
-	      dev_id = Developer.where(email: em[0]).limit(1).pluck(:id)[0]
-	      owner_email = em[0]
-	      commitHash = line[2]
-	      date = line[3]
-	      directory = line[1]
-	      ownership << [owner_email, dev_id, directory, commitHash, date]
-		end
+	    id = Developer.where(email: em[0]).limit(1).pluck(:id)[0]
+	    ReleaseOwner.where(dev_id: id , owner_email: em[0], directory: line[1]).update_all(first_ownership_sha: line[2], first_ownership_date: line[3])
 	  else
 	    abort ("Failed to clear email: #{line[0]}")
 	  end
     end
-    ownership.fsync
-	ActiveRecord::Base.connection.execute("COPY first_ownerships FROM '#{tmp}/first-ownership.csv' DELIMITER ',' CSV")
-  end
-  
-  def related?(parent, subdirs)
-    if parent.eql?("./")
-	  return true
-	end
-    subdirs.each do |subdir|
-	  Pathname.new(subdir).ascend do |s|
-	  s = "#{s}/"
-        if(parent.eql? s)
-          return true
-	    end
-	  end
-	end
-	return false
   end
 end
