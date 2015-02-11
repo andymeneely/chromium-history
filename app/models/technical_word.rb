d9e5f547d921564c87a5516e5c53586c5547318f
bspates <bxs4361@g.rit.edu>
2015-02-11 12:50:36 -0500

    update technical word associations

--- app/models/technical_word.rb
+++ app/models/technical_word.rb
@@ -1,7 +1,7 @@
 class TechnicalWord < ActiveRecord::Base
   
-  has_and_belongs_to_many :code_reviews
-  has_and_belongs_to_many :developers
+  has_and_belongs_to_many :messages
+  has_and_belongs_to_many :comments
 
   def self.on_optimize
     ActiveRecord::Base.connection.add_index :word
