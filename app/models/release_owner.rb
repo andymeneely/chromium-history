bd89d4a599cd13618c518ec8b102938811e0d4f0
Andy Meneely <andy@se.rit.edu>
2015-02-20 14:14:38 -0500

    Changing the index structure

--- app/models/release_owner.rb
+++ app/models/release_owner.rb
@@ -2,13 +2,14 @@ class ReleaseOwner < ActiveRecord::Base
   belongs_to :release, foreign_key: 'release', primary_key: 'name'
   belongs_to :filepath, foreign_key: 'filepath', primary_key: 'filepath'
   belongs_to :developer, foreign_key: 'dev_id', primary_key: 'id'
-  has_many :commits, through: :developer
   
   def self.optimize
     connection.add_index :release_owners, :release
     connection.add_index :release_owners, :filepath
-	connection.add_index :release_owners, :directory
+	  connection.add_index :release_owners, :directory
+    connection.add_index :release_owners, :dev_id
+    connection.add_index :release_owners, [:directory, :dev_id]
     connection.add_index :release_owners, [:release,:filepath,:directory]
-    connection.execute "CLUSTER release_owners USING index_release_owners_on_release_and_filepath_and_directory"
+    connection.execute "CLUSTER release_owners USING index_release_owners_on_directory_and_dev_id"
   end
 end
