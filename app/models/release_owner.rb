5cebbe72cb52efe0f9ba8e2c649d5c53a8285cd7
Andy Meneely <andy@se.rit.edu>
2015-03-03 16:27:54 -0500

    Disabling some indexes

--- app/models/release_owner.rb
+++ app/models/release_owner.rb
@@ -4,12 +4,11 @@ class ReleaseOwner < ActiveRecord::Base
   belongs_to :developer, foreign_key: 'dev_id', primary_key: 'id'
   
   def self.optimize
-    connection.add_index :release_owners, :release
     connection.add_index :release_owners, :filepath
-	  connection.add_index :release_owners, :directory
-    connection.add_index :release_owners, :dev_id
+	  #connection.add_index :release_owners, :directory
+    #connection.add_index :release_owners, :dev_id
     connection.add_index :release_owners, [:directory, :dev_id]
-    connection.add_index :release_owners, [:release,:filepath,:directory]
+    #connection.add_index :release_owners, [:release,:filepath,:directory]
     #connection.execute "CLUSTER release_owners USING index_release_owners_on_directory_and_dev_id"
   end
   
