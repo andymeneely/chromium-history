d5e6d72a6968d746ac31d7d87952802ea26ddb09
Andy Meneely <andy@se.rit.edu>
2015-03-13 00:17:37 -0400

    Fixing vocab_verify

--- lib/verify/production/vocab_verify.rb
+++ lib/verify/production/vocab_verify.rb
@@ -1,6 +1,6 @@
 require_relative '../verify_base.rb'
 
-class ProductionVocsbVerify < VerifyBase
+class VocabVerify < VerifyBase
   def verify_no_auto_words
     assert_equal 0, TechnicalWord.where(word: 'rightli').count
   end
