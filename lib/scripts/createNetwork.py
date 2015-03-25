d7497b2b4aeeec12dddbb16277106f01a03e397b
Samantha Oxley <sso7159@artifacts.gccis.rit.edu>
2015-03-24 23:11:45 -0400

    Add createNetwork.py

--- lib/scripts/createNetwork.py
+++ lib/scripts/createNetwork.py
@@ -23,9 +23,9 @@ except psycopg2.DatabaseError, e:
 # we will need to create an array of graph objects 
 # each graph object will be for a specific time frame
 graphArray = []		
-earlyBoundary = '2005-01-01 00:00:00.000000'
+earlyBoundary = '2006-01-01 00:00:00.000000'
 earlyTime = datetime.strptime( earlyBoundary, "%Y-%m-%d %H:%M:%S.%f")
-lateBoundary = '2006-01-01 00:00:00.000000'
+lateBoundary = '2006-04-01 00:00:00.000000'
 lateTime = datetime.strptime( lateBoundary, "%Y-%m-%d %H:%M:%S.%f")
 while earlyBoundary < '2015-01-01 00:00:00.000000':
 	G = nx.MultiGraph()
@@ -42,23 +42,43 @@ while earlyBoundary < '2015-01-01 00:00:00.000000':
 	except psycopg2.DatabaseError, e:
 		print 'Error %s' % e
 		sys.exit(1)
-	print 'IN TIME PHRAME: %s - %s \n' % (earlyBoundary, lateBoundary)
+	print '-------------IN TIME FRAME: %s - %s ---------\n' % (earlyBoundary, lateBoundary)
 	print G.edges(data=True)
-	print '\n'	
+	print '----------------------------------------------\n'	
 	# change boundaries and add G to array of graph
-	lp_yr = lateTime.year
+	#molen = lateTime.
 	earlyTime = lateTime
-	if (lp_yr % 4) == 0:
-		lateTime += timedelta(days=366)
-	else:
-		lateTime += timedelta(days=365)
+	#if (lp_yr % 4) == 0:
+	#	lateTime += timedelta(days=366)
+	#else:
+	lateTime += timedelta(days=90)
 	earlyBoundary = earlyTime.strftime("%Y-%m-%d %H:%M:%S.%f")
 	lateBoundary = lateTime.strftime("%Y-%m-%d %H:%M:%S.%f")
 	graphArray.append(G)
 # close the connection to the database
 if con:
 	con.close()
-	print '++++++++++\nThese are the connected components of each graph\n'
-#for gr in graphArray:
-#	print gr.edges(data=True)
+	#print '++++++++++\nThese are the connected components of each graph\n'
+for gr in graphArray:
+	#print gr.edges(data=True)
+	node_deg = gr.degree()
+	print node_deg
+	max_deg1 = 0
+	max_deg2 = 0
+	max_deg3 = 0
+	#find highest three degree values for each time period
+	for key in sorted(node_deg, key=node_deg.get, reverse=True):
+		if max_deg1 == 0:
+			max_deg1 = key
+		elif max_deg2 == 0:
+			max_deg2 = key
+		elif max_deg3 == 0:
+			max_deg3 = key
+	print "Maximum degrees for this period: %d, %d, %d" %( max_deg1, max_deg2, max_deg3 )	
+		
+
+
+
+
+
 
