67aa5f06d1c76bafcee69ef31089bcfe0f3fa173
Richard Kalimba <rik1209@rit.edu>
2015-03-27 09:38:57 -0400

    clean up obsolete methods

--- app/models/release_owner.rb
+++ app/models/release_owner.rb
@@ -12,18 +12,6 @@ class ReleaseOwner < ActiveRecord::Base
     #connection.execute "CLUSTER release_owners USING index_release_owners_on_directory_and_dev_id"
   end
   
-  def time_to_ownership
-    first_ownership_date - first_dir_commit_date
-  end
-  
-  def ownership_time_at_release
-	release.date - first_ownership_date
-  end
-  
-  def ownership_distance
-    File.dirname(filepath.filepath).split(/\.\/|\//).count - directory.split(/\.\/|\//).count
-  end
-  
   def committed_filepaths_to_ownership
     self.directory = "" if self.directory.split(/\.\/|\//).count == 0
     return Commit.joins(:commit_filepaths).where(commit_hash:  CommitFilepath.where("filepath LIKE ?", "#{self.directory}%").pluck(:commit_hash), author_id: dev_id).where("created_at < ?", first_ownership_date).group(:filepath).count
