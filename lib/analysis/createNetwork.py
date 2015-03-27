75456378a16b2008704aa1d4192131afeb5a2164
Samantha Oxley <sso7159@artifacts.gccis.rit.edu>
2015-03-27 17:01:43 -0400

    Add createNework.py

--- lib/analysis/createNetwork.py
+++ lib/analysis/createNetwork.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # Samantha Oxley & Kayla Davis
-# connect to the psql database
+# connect to the psql database, create a list of graph objects
+# ARGUMENTS: [username] [database name]
 
 import psycopg2
 import sys, getopt
@@ -11,26 +12,22 @@ from datetime import datetime, timedelta
 
 # Get the username and db from command line
 username, db = sys.argv[1], sys.argv[2]
-
 # connection to database 
 con = None
 try:
 	con = psycopg2.connect(database=db, user=username)
 	cur = con.cursor()
-	#cur.execute('SELECT version()');
-	#ver = cur.fetchone()
-	#print ver
 except psycopg2.DatabaseError, e:
 	print 'Error %s' % e
 	sys.exit(1)
 # we will need to create an array of graph objects 
 # each graph object will be for a specific time frame
 graphArray = []		
-earlyBoundary = '2006-01-01 00:00:00.000000'
+earlyBoundary = '2008-09-01 00:00:00.000000'
 earlyTime = datetime.strptime( earlyBoundary, "%Y-%m-%d %H:%M:%S.%f")
-lateBoundary = '2006-04-01 00:00:00.000000'
+lateBoundary = '2008-12-01 00:00:00.000000'
 lateTime = datetime.strptime( lateBoundary, "%Y-%m-%d %H:%M:%S.%f")
-while earlyBoundary < '2015-01-01 00:00:00.000000':
+while earlyBoundary < '2014-11-15 00:00:00.000000':
 	G = nx.MultiGraph()
 	# query for this time boundary
 	try:
@@ -41,35 +38,35 @@ while earlyBoundary < '2015-01-01 00:00:00.000000':
 			row_count += 1
 			# add each edge between two nodes, with the issue associated
 			G.add_edge( row[1], row[2], issue=row[3] )
-			#print 'edge added for %d + %d on %d\n' % (row[1],row[2],row[3])
 	except psycopg2.DatabaseError, e:
 		print 'Error %s' % e
 		sys.exit(1)
-	print '-------------IN TIME FRAME: %s - %s ---------\n' % (earlyBoundary, lateBoundary)
-	#print G.edges(data=True) # disabled becuase this is SUPER verbose on chromium_real (Andy)
-	print '----------------------------------------------\n'	
 	# change boundaries and add G to array of graph
-	#molen = lateTime.
 	earlyTime = lateTime
-	#if (lp_yr % 4) == 0:
-	#	lateTime += timedelta(days=366)
-	#else:
-	lateTime += timedelta(days=90)
+	lateTime += timedelta(days=61)
 	earlyBoundary = earlyTime.strftime("%Y-%m-%d %H:%M:%S.%f")
 	lateBoundary = lateTime.strftime("%Y-%m-%d %H:%M:%S.%f")
 	graphArray.append(G)
 # close the connection to the database
 if con:
 	con.close()
-	#print '++++++++++\nThese are the connected components of each graph\n'
 for gr in graphArray:
+	# THIS IS EXTREMELY VERBOSE ON CHROMIUM_REAL, FOR DEV DATA ONLY
+	# GET LIST OF EDGES WITH THE ISSUE LISTED AS WELL
+	#print '++++++++++\nThese are the connected components of each graph\n' 
 	#print gr.edges(data=True)
+
+	# THIS IS TO SEE A DICTIONARY OF EACH DEV_ID(key)TO THE DEGREE OF THAT DEV(value)
+	print '\n--------\n'
 	node_deg = gr.degree()
 	print node_deg
+	# FIND THE FIVE HIGHEST DEGREES
 	max_deg1 = 0
 	max_deg2 = 0
 	max_deg3 = 0
-	#find highest three degree values for each time period
+	max_deg4 = 0
+	max_deg5 = 0
+	#find highest five degree values for each time period
 	for key in sorted(node_deg, key=node_deg.get, reverse=True):
 		if max_deg1 == 0:
 			max_deg1 = key
@@ -77,11 +74,8 @@ for gr in graphArray:
 			max_deg2 = key
 		elif max_deg3 == 0:
 			max_deg3 = key
-	print "Maximum degrees for this period: %d, %d, %d" %( max_deg1, max_deg2, max_deg3 )	
-		
-
-
-
-
-
-
+		elif max_deg4 == 0:
+			max_deg4 = key
+		elif max_deg5 == 0:
+			max_deg5 = key
+	print "Maximum degrees for this period: %d:%d, %d:%d, %d:%d, %d:%d, %d:%d" %( max_deg1, node_deg[max_deg1], max_deg2, node_deg[max_deg2], max_deg3, node_deg[max_deg3], max_deg4, node_deg[max_deg4], max_deg5, node_deg[max_deg5])
