cb3f81411abe87ce37c1c3005972bc2a2334e970
Samantha Oxley <sso7159@artifacts.gccis.rit.edu>
2015-04-20 15:21:32 -0400

    Network analysis updated to skim developer sheriff hrs, own%, bug&sec experience... runs very slowly.. improvements in progress

--- lib/analysis/createNetwork.py
+++ lib/analysis/createNetwork.py
@@ -12,6 +12,7 @@ import json
 import networkx as nx
 from networkx.readwrite import json_graph
 from datetime import datetime, timedelta
+from collections import OrderedDict
 
 # Get the username and db from command line
 username, db = sys.argv[1], sys.argv[2]
@@ -30,20 +31,36 @@ earlyBoundary = '2008-09-01 00:00:00.000000'
 earlyTime = datetime.strptime( earlyBoundary, "%Y-%m-%d %H:%M:%S.%f")
 lateBoundary = '2008-12-01 00:00:00.000000'
 lateTime = datetime.strptime( lateBoundary, "%Y-%m-%d %H:%M:%S.%f")
+
+fileCount = 1
+
 while earlyBoundary < '2014-11-15 00:00:00.000000':
-	G = nx.MultiGraph()
+	G = nx.MultiGraph(begin=earlyBoundary,end=lateBoundary)
 	# query for this time boundary
 	try:
 		string = "SELECT * FROM adjacency_list WHERE review_date >= '" + earlyBoundary + "' AND review_date < '" + lateBoundary + "'";
 		cur.execute(string)
-		row_count = 0 
 		for row in cur:
-			row_count += 1
 			# add each edge between two nodes, with the issue associated
 			G.add_edge( row[1], row[2], issue=row[3] )
 	except psycopg2.DatabaseError, e:
 		print 'Error %s' % e
 		sys.exit(1)
+	# THIS CODE IS USED TO GENERATE COMMA-DELIMITED TEXT FILES WHICH CAN BE 
+	# EASILY IMPORTED INTO EXCEL FOR DISTRIBUTION ANALYZATION
+	# DEGREE DISTRIBUTION FOLLOWS POWER LAW
+	# adds this new graph's node_ids and degrees to a file
+	#node_deg = G.degree()
+	# write information for this time period to the text file	
+	#theFile = open( "../../../../graph_degree_files/graph" +str(fileCount)+ ".txt" , 'w' ) 
+	#theFile.write( "dev_id,dev_degree," +earlyBoundary+ "," +lateBoundary+ "\n" )
+	#for key in sorted(node_deg, key=node_deg.get, reverse=True):
+		#count_node += 1
+		#avg_deg += node_deg[key]
+	#	theFile.write( str(key) +","+ str(node_deg[key])+ "\n" )
+	#theFile.close()	
+	#fileCount = fileCount + 1
+	
 	# change boundaries and add G to array of graph
 	earlyTime = lateTime
 	lateTime += timedelta(days=61)
@@ -51,131 +68,101 @@ while earlyBoundary < '2014-11-15 00:00:00.000000':
 	lateBoundary = lateTime.strftime("%Y-%m-%d %H:%M:%S.%f")
 	graphArray.append(G)
 # close the connection to the database
-if con:
-	con.close()
+#if con:
+#	con.close()
 grnum = 1
 
 for gr in graphArray:
-	# THIS IS EXTREMELY VERBOSE ON CHROMIUM_REAL, FOR DEV DATA ONLY
-	# GET LIST OF EDGES WITH THE ISSUE LISTED AS WELL
-	#print '++++++++++\nThese are the connected components of each graph\n' 
-	#print gr.edges(data=True)
-	keys_in_1SD = []
-	keys_in_2SD = []
-	keys_in_3SD = []
-	keys_higher_3SD = []
-	keys_lower_3SD = []
-	# THIS IS TO SEE A DICTIONARY OF EACH DEV_ID(key)TO THE DEGREE OF THAT DEV(value)
-	#print '\n--------\n'
+	# return a dictionary of this graph's node degrees
 	node_deg = gr.degree()
-	#print node_deg
-	# FIND THE FIVE HIGHEST DEGREES
-	max_deg1 = 0
-	max_deg2 = 0
-	max_deg3 = 0
-	max_deg4 = 0
-	max_deg5 = 0
-	# initialize values to 0 
-	avg_deg = 0
-	count_node = 0
-	variance = 0
-	std_dev = 0
-	oneSD_up = 0
-	oneSD_down = 0
-	twoSD_up = 0
-	twoSD_down = 0
-	thrSD_up = 0
-	thrSD_down = 0
-	#find maximum degree values and begin adding for mean
-	for key in sorted(node_deg, key=node_deg.get, reverse=True):
-		count_node += 1
-		avg_deg += node_deg[key]
-		if max_deg1 == 0:
-			max_deg1 = key
-		elif max_deg2 == 0:
-			max_deg2 = key
-		elif max_deg3 == 0:
-			max_deg3 = key
-		elif max_deg4 == 0:
-			max_deg4 = key
-		elif max_deg5 == 0:
-			max_deg5 = key
-	print "For this time period : "
-	# just to clear up when this is run on developer data
-	# Max_deg is outdated now that we have a standard deviation
-	#if max_deg1 == 0 or max_deg2 == 0 or max_deg3 == 0 or max_deg4 == 0 or max_deg5 == 0:
-	#	print "Max degrees not calculated, sample sufficiently small."
-	#else:
-	#	print "Maximum degrees for this period: %d:%d, %d:%d, %d:%d, %d:%d, %d:%d." %( max_deg1, node_deg[max_deg1], max_deg2, node_deg[max_deg2], max_deg3, node_deg[max_deg3], max_deg4, node_deg[max_deg4], max_deg5, node_deg[max_deg5])
-	
-	if count_node != 0:
-		# calc average degree (mean) and variance
-		avg_deg = avg_deg / count_node
-		for key in sorted( node_deg, key=node_deg.get, reverse=True):
-			variance += (node_deg[key]-avg_deg)*(node_deg[key]-avg_deg)	
-		variance = variance / count_node
-		# calc standard deviations, lower cannot be negative 
-		std_dev = sqrt(variance) 	
-		oneSD_up = avg_deg + std_dev
-		oneSD_down = avg_deg - std_dev
-		if oneSD_down < 0:
-			oneSD_down = 0
-		twoSD_up = avg_deg + (2*std_dev)
-		twoSD_down = avg_deg - (2*std_dev)
-		if twoSD_down < 0:
-			twoSD_down = 0
-		thrSD_up = avg_deg + (3*std_dev)
-		thrSD_down = avg_deg - (3*std_dev)
-		if thrSD_down < 0:
-			thrSD_down = 0
-		print "Mean: %d, Variance: %d, Std Dev: %d" %( avg_deg, variance, std_dev )
-		print "Range within 1SD: %d-%d, 2SD: %d-%d, 3SD: %d-%d" %( oneSD_up, oneSD_down, twoSD_up, twoSD_down, thrSD_up, thrSD_down) 
-		for key in sorted( node_deg, key=node_deg.get, reverse=True):
-			if node_deg[key] > thrSD_up:
-				keys_higher_3SD.append(key)
-			else:
-				if node_deg[key] < thrSD_up and node_deg[key] > thrSD_down:
-					keys_in_3SD.append(key)
-				if node_deg[key] < twoSD_up and node_deg[key] > twoSD_down:
-					keys_in_2SD.append(key)
-				if node_deg[key] < oneSD_up and node_deg[key] > oneSD_down:
-					keys_in_1SD.append(key)
-			if thrSD_down != 0:
-				if node_deg[key] < thrSD_down:
-					keys_lower_3SD.append(key)
-		print "Now listing degrees above 3 standard deviations: "
-		for i in keys_higher_3SD:
-			prnt = str(i) + ":" + str(node_deg[i]) + " ,"
-			sys.stdout.write( prnt )
-		print "\nNow listing degrees within 3 SD but not within 2 SD: "
-		for i in keys_in_3SD:
-			if i in keys_in_2SD:
-				break
-			prnt = str(i) + ":" + str(node_deg[i]) + " ,"
-			sys.stdout.write( prnt )		
-		print "\nNow listing degrees within 2 SD but not within 1 SD: "
-		for i in keys_in_2SD:
-			if i in keys_in_1SD:
-				break
-			prnt = str(i) + ":" + str(node_deg[i]) + " ,"
-			sys.stdout.write( prnt )
-		print "\nNow listing degrees within 1 standard deviations: "
-		#for i in keys_in_1SD:
-		#	prnt = str(i) + ":" + str(node_deg[i]) + " ,"
-		#	sys.stdout.write( prnt )
-		print "Too verbose, %d degrees are within 1 SD" %( len(keys_in_1SD) )
-		print "Now listing degrees below 3 standard deviations: "
-		for i in keys_lower_3SD:
-			prnt = str(i) + ":" + str(node_deg[i]) + " ,"
-			sys.stdout.write( prnt )	
-	# NEXT STEP: connect each node (developer id) to the issues they had in this time period
-	# check the % of those issues that had bugs/vulnerabilities
-	# calculate the average % for each std_dev 
+	num_nodes = len(node_deg)
+	if num_nodes == 0:
+		continue
+	# move the node degree items into an ascending list of degrees for quartile ranges
+	sorted_deg = OrderedDict( sorted( node_deg.items(), key=lambda(k,v):(v,k) ) )
+	#length = len(sorted_deg)
+	median = 0
+	low_quart = 0
+	upper_quart = 0
+	outlier_quart = 0
+	sorted_deg_values = sorted_deg.values()
+	if ( num_nodes % 2 ) == 0:
+		mid_lo = (num_nodes / 2) - 1
+		mid_hi = num_nodes / 2
+		median = (sorted_deg_values[mid_lo] + sorted_deg_values[mid_hi])/2
 	else:
-		print "No nodes to analyze"
+		median = sorted_deg_values[ ((num_nodes + 1) / 2)-1 ]
+	low_mid = int( round( (num_nodes+1)/4 )-1 )
+	low_quart = sorted_deg_values[low_mid]
+	high_mid = int( round( (num_nodes-1)*0.75 ) )
+	upper_quart = sorted_deg_values[high_mid]
+	outlier_quart = upper_quart * 3
+	print "\nFor graph #" + str(grnum) + " IN TIME FRAME: " + gr.graph["begin"] + " UNTIL " + gr.graph["end"]
+	print "lower q:%d , median: %d, upper q:%d, outlier q:%d" %( low_quart, median, upper_quart, outlier_quart) 
+	grnum = grnum + 1
+
+	# now group the developers by their degree values
+	qrt1_devs = []
+	qrt2_devs = []
+	qrt3_devs = []
+	qrt4_devs = []
+	qrto_devs = []	
+	#print "dev and nodes: "
+	#print node_deg
+	for dev in sorted_deg:	
+		gr.node[dev]["degree"] = sorted_deg[dev]
+		if sorted_deg[dev] <= low_quart:
+			qrt1_devs.append(dev)	
+		elif sorted_deg[dev] <= median:
+			qrt2_devs.append(dev)
+		elif sorted_deg[dev] <= upper_quart:
+			qrt3_devs.append(dev)
+		elif sorted_deg[dev] <= outlier_quart:
+			qrt4_devs.append(dev)
+		else:
+			qrto_devs.append(dev) 
+	#	sys.stdout.write(str(dev) + ": "+ str(gr.node[dev]["degree"]) + ",")
+	#print "quartile 1:"
+	#for elem in qrt1_devs:
+	#	sys.stdout.write( str(elem) + "," )
+	#print "\nquartile 2:"
+	#for elem in qrt2_devs:
+	#	sys.stdout.write( str(elem) + "," )
+	#print "\nquartile 3:"
+	#for elem in qrt3_devs:
+	#	sys.stdout.write( str(elem) + "," )
+	earlyBound = gr.graph["begin"]
+	lateBound = gr.graph["end"]
+	#print "total number of devs: " + str(len(nodes)) +"- qrt1: " + str(len(qrt1_devs)) + " qrt2: " + str(len(qrt2_devs)) + " qrt3: " + str(len(qrt3_devs)) + " outliers: " + str(len(qrto_devs))
+	for elem in qrt1_devs:
+		#qry_str = "SELECT owner_id, issue, created FROM code_reviews WHERE owner_id = "+ str(elem) + " AND created > " + earlyBound + " AND created <= " + lateBound	
+		qry_str = "SELECT p1.dev_id, p1.owner_id, p1.issue, p1.review_date, p1.sheriff_hours, p1.security_experienced, p1.bug_security_experienced "
+		qry_str = qry_str + "FROM participants p1 INNER JOIN participants p2 ON (p1.issue = p2.issue AND p1.dev_id < p2.dev_id )"
+		qry_str = qry_str + " WHERE (p1.dev_id = " +str(elem) +" OR p2.dev_id = " +str(elem)+") AND  p1.review_date > '" + str(earlyBound) + "' AND p1.review_date <= '" + str(lateBound) + "'"
+		cur.execute(qry_str)
+		rows = 0
+		shr_hrs = 0
+		owns = 0
+		sec_exp = 0
+		bug_exp = 0
+		for row in cur:
+			rows = rows + 1
+			shr_hrs = row[4]
+			if row[0] == row[1]:
+				owns = owns + 1
+			sec_exp = row[5]
+			bug_exp = row[6] 
+		if rows != 0:
+			owns = int(round( (float(owns)/float(rows)), 2) * 100)
+		print "dev_id: "+str(elem)+", deg: "+str(gr.node[elem]["degree"])+", cols:"+str(rows)+" shr_hrs:"+str(shr_hrs)+" own(%):"+str(owns)+" sec/bug exp:"+str(sec_exp)+"/"+str(bug_exp)
+		#sys.stdout.write( str(elem) + ":" + str(node_deg.get(elem)) + "," )
+
+# Kayla Stuff
 	#make data json and dump to file
 	#data = json_graph.node_link_data(gr)
 	#with open('jsongraphs/data' + str(grnum) + '.json', 'w') as outfile:
    	#	json.dump(data, outfile)
-	grnum += 1
-	print "*************************************"
+	#grnum += 1
+if con:
+	con.close()	
+#print "*************************************"
