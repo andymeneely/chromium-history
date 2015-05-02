bcd4639d0a6ef629df322892b7686fab06dedcee
Richard Kalimba <rik1209@rit.edu>
2015-05-01 23:22:24 -0400

    Removed dead code, adding top tech words per label
    Includes R analysis on tech words, their weights in each label
    Identifies top 20 tech words for each label by weight (tfidf)

--- lib/analysis/nlp_queries_analysis.rb
+++ lib/analysis/nlp_queries_analysis.rb
@@ -1,17 +1,126 @@
-require 'hirb'
+require 'rinruby'
 
 class NlpQueriesAnalysis
 
-  def run
-    
-    #create table with label/tech word use frequencies, 0 initially
-    drop_label_word_freqs_table = 'DROP TABLE IF EXISTS label_word_freqs'
-    create_label_word_freqs_table = <<-EOSQL
-      CREATE TABLE label_word_freqs AS (
-        SELECT label, word, 0 as freq FROM labels CROSS JOIN technical_words
-      )
-    EOSQL
+  def initialize
+    R.echo false, false
+  end
 
+  def run
+    #create the technical word use in msgs and reviews tables
+    create_freq_tables
+	
+    connect_db
+
+    #get the technical words and use weight in msgs and reviews per associated labels
+    technical_messages_vs_buglabels
+    technical_reviews_vs_buglabels
+	
+    #from the weighted word-use tables, get only the top-ranked 20 words in use for each label_id
+    create_top_tech_words_tables
+    close_db
+  end
+  
+  def connect_db
+    conf = Rails.configuration.database_configuration[Rails.env]
+    #To install these packages locally, run R on the command line
+    # and then run
+    # >>> install.packages("DBI")
+    # >>> install.packages("RPostgreSQL")
+    # >>> install.packages("ROCR")
+    # >>> install.packages("bestglm")
+    # >>> install.packages("lsr")
+    # >>> install.packages("reshape2")
+    # >>> install.packages("tm")
+    #
+    # If tm doesn't not install due to issues with R version, download old package archive
+    # and install it directly from a source directory:
+    # archives_url: http://cran.r-project.org/src/contrib/Archive/tm/ , Now using: tm_0.5-9.1.tar.gz
+    #
+    # In teminal type: wget "replace with a source url from archives". Downloads archive to current dir
+    # Open R and run:
+    # >>> install.packages(path_to_downloaded_src_archive, repos=NULL, type="source")
+    R.eval <<-EOR
+      library(DBI)
+      library(RPostgreSQL)
+      library(reshape2)
+      library(slam)
+      library(tm)
+      drv <- dbDriver("PostgreSQL")
+      con <- dbConnect(drv, 
+                       user="#{conf['username']}", 
+                       password="#{conf['password']}", 
+                       dbname="#{conf['database']}")
+    EOR
+  end
+
+  def close_db
+    R.eval <<-EOR
+      dbDisconnect(con)
+      dbUnloadDriver(drv)
+    EOR
+  end
+  
+  def technical_messages_vs_buglabels
+    R.eval <<-EOR
+      #get the frequency data from tables in the db
+      msgfreqdata <- dbReadTable(con,'labtechmsgfreqs')
+	
+      #convert the freqs data frames to matrices
+      msgfreqmatrix <- acast(msgfreqdata,label_id~word_id,value.var='freq')
+    
+      #remove na values, replace them with 0
+      msgfreqmatrix[is.na(msgfreqmatrix)] <- 0
+	
+      #convert the frequency matrices to DocumentTermMatrix, Labels as document, and tech words as the terms, using tfidf as word weight
+      msgdtm <- as.DocumentTermMatrix(msgfreqmatrix,weightTfIdf)
+	
+      #convert to matrix with term weights
+      msgdtm2 <- as.matrix(msgdtm)
+
+      #remove unused words
+      msgdtm2[msgdtm2 == 0] <- NA
+
+      #convert the matrices into data frames to save
+      label_msg_top_words <- melt(msgdtm2,varnames = c('label_id','word_id'),na.rm=TRUE,value.name='weightTfIdf')
+
+      #save matrices as tables
+      names(label_msg_top_words) <- tolower(names(label_msg_top_words))
+      dbWriteTable(con,'label_msg_top_words',label_msg_top_words,row.names=FALSE,overwrite=TRUE)
+    EOR
+  end
+  
+  def technical_reviews_vs_buglabels
+    R.eval <<-EOR
+      #get the frequency data from tables in the db
+      revfreqdata <- dbReadTable(con,'labtechrevfreqs')
+	
+      #convert the freqs data frames to matrices
+      revfreqmatrix <- acast(revfreqdata,label_id~word_id,value.var='freq')
+	
+      #remove na values, replace them with 0
+      revfreqmatrix[is.na(revfreqmatrix)] <- 0
+	
+      #convert the frequency matrices to DocumentTermMatrix, Labels as document, and tech words as the terms, using tfidf as word weight
+      revdtm <- as.DocumentTermMatrix(revfreqmatrix,weightTfIdf)
+	
+      #convert to matrix with term weights
+      revdtm2 <- as.matrix(revdtm)
+
+      #remove unused words
+      revdtm2[revdtm2 == 0] <- NA
+
+      #convert the matrices into data frames to save
+      label_rev_top_words <- melt(revdtm2,varnames = c('label_id','word_id'),na.rm=TRUE,value.name='weightTfIdf')
+
+      #save matrices as tables
+      names(label_rev_top_words) <- tolower(names(label_rev_top_words))
+      dbWriteTable(con,'label_rev_top_words',label_rev_top_words,row.names=FALSE,overwrite=TRUE)
+    EOR
+  end
+  
+  def create_freq_tables
+  
     #create a table with counts for technical messages by label & technical word
     drop_label_tech_msgs_table = 'DROP TABLE IF EXISTS label_tech_msgs'
     create_label_tech_msgs_table = <<-EOSQL
@@ -46,92 +155,49 @@ class NlpQueriesAnalysis
     EOSQL
 
     #create a table with frequencies for label/tech word use in messages
-    drop_label_techmsgs_freqs = 'DROP TABLE IF EXISTS label_techmsgs_freqs'
-    create_label_techmsgs_freqs = <<-EOSQL
-      CREATE TABLE label_techmsgs_freqs AS (
-        SELECT t.label AS label, t.word AS word, (CASE WHEN t2.freq IS NULL THEN t.freq ELSE t2.freq END) AS freq 
-	FROM label_word_freqs t LEFT OUTER JOIN label_tech_msgs t2 ON (t.label = t2.label AND t.word = t2.word) 
-        ORDER BY label, word, freq)
+    drop_labtechmsgfreqs = 'DROP TABLE IF EXISTS labtechmsgfreqs'
+    create_labtechmsgfreqs = <<-EOSQL
+      CREATE TABLE labtechmsgfreqs AS (
+	    SELECT l.label_id AS label_id, tw.id AS word_id, ltm.freq AS freq FROM label_tech_msgs ltm INNER JOIN labels l ON l.label = ltm.label
+		                                                                                       INNER JOIN technical_words tw ON tw.word = ltm.word
+      )
     EOSQL
     
     #create a table with frequencies for label/tech word use in reviews
-    drop_label_techrevs_freqs = 'DROP TABLE IF EXISTS label_techreviews_freqs'
-    create_label_techrevs_freqs = <<-EOSQL
-      CREATE TABLE label_techreviews_freqs AS (
-        SELECT t.label AS label, t.word AS word, (CASE WHEN t2.freq IS NULL THEN t.freq ELSE t2.freq END) AS freq 
-	FROM label_word_freqs t LEFT OUTER JOIN label_tech_reviews t2 ON (t.label = t2.label AND t.word = t2.word) 
-        ORDER BY label, word, freq)
+    drop_labtechrevfreqs = 'DROP TABLE IF EXISTS labtechrevfreqs'
+    create_labtechrevfreqs = <<-EOSQL
+      CREATE TABLE labtechrevfreqs AS (
+	    SELECT l.label_id AS label_id, tw.id AS word_id, ltm.freq AS freq FROM label_tech_reviews ltm INNER JOIN labels l ON l.label = ltm.label
+		                                                                                          INNER JOIN technical_words tw ON tw.word = ltm.word
+      )
     EOSQL
 
     Benchmark.bm(40) do |x|
-      x.report("Executing drop label_techmsgs_freqs table") {ActiveRecord::Base.connection.execute drop_label_word_freqs_table}
-      x.report("Executing create label_techmsgs_freqs table") {ActiveRecord::Base.connection.execute create_label_word_freqs_table}
+      x.report("Executing drop label_techmsgs cnts") {ActiveRecord::Base.connection.execute drop_label_tech_msgs_table}
+      x.report("Executing create label_techmsgs cnts") {ActiveRecord::Base.connection.execute create_label_tech_msgs_table}
       
-      x.report("Executing drop label_techmsgs counts table") {ActiveRecord::Base.connection.execute drop_label_tech_msgs_table}
-      x.report("Executing create label_techmsgs counts table") {ActiveRecord::Base.connection.execute create_label_tech_msgs_table}
-      
-      x.report("Executing drop label_techrevs counts table") {ActiveRecord::Base.connection.execute drop_label_tech_reviews_table}
-      x.report("Executing create label_techrevs counts table") {ActiveRecord::Base.connection.execute create_label_tech_reviews_table}
+      x.report("Executing drop label_techrevs cnts") {ActiveRecord::Base.connection.execute drop_label_tech_reviews_table}
+      x.report("Executing create label_techrevs cnts") {ActiveRecord::Base.connection.execute create_label_tech_reviews_table}
 
-      x.report("Executing drop label_techmsgs_freqs table") {ActiveRecord::Base.connection.execute drop_label_techmsgs_freqs}
-      x.report("Executing create label_techmsgs_freqs table") {ActiveRecord::Base.connection.execute create_label_techmsgs_freqs}
+      x.report("Executing drop labtechmsgfreqs table") {ActiveRecord::Base.connection.execute drop_labtechmsgfreqs}
+      x.report("Executing create labtechmsgfreqs table") {ActiveRecord::Base.connection.execute create_labtechmsgfreqs}
 
-      x.report("Executing drop label/tech reviews_freqs table") {ActiveRecord::Base.connection.execute drop_label_techrevs_freqs}
-      x.report("Executing create label/tech reviews freqs table") {ActiveRecord::Base.connection.execute create_label_techrevs_freqs}
+      x.report("Executing drop labtechrevfreqs table") {ActiveRecord::Base.connection.execute drop_labtechrevfreqs}
+      x.report("Executing create labtechrevfreqs table") {ActiveRecord::Base.connection.execute create_labtechrevfreqs}
     end
-=begin
-    puts
-    puts "_____Results from the nlp queries______"
-    Hirb.enable
-
-    puts "Technical words associated with too many labels: "
-    puts
-    puts Hirb::Helpers::AutoTable.render (TechnicalWord.joins(messages: {code_review: {commit: {commit_bugs: {bug: :labels}}}}).group("technical_words.id").order("COUNT(labels.label) DESC").limit(50).select("technical_words.id",:word,"COUNT(labels.label)"))
-
-    #puts "Top word-bug label pairs: "
-    #puts
-    #puts Hirb::Helpers::AutoTable.render (TechnicalWord.joins(messages: {code_review: {commit: {commit_bugs: {bug: :labels}}}}).group("technical_words.id","labels.label").order("COUNT(*) DESC").limit(50).select(:word,"labels.label","COUNT(*)"))
-	
-    puts "Top technical words in reviews for commits associated to bugs"
-    puts
-    puts Hirb::Helpers::AutoTable.render (CodeReview.joins(messages: :technical_words).where(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).group(:word).order('COUNT(code_reviews.issue) DESC').limit(50).select(:word,'COUNT(code_reviews.issue)'))
-	
-    puts "Top technical words in reviews for commits NOT associated to bugs"
-    puts
-    puts Hirb::Helpers::AutoTable.render (CodeReview.joins(messages: :technical_words).where.not(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).group(:word).order('COUNT(code_reviews.issue) DESC').limit(50).select(:word,'COUNT(code_reviews.issue)'))
-	
-    puts "Average technical words in reviews for commits with bug associations"
-    puts
-    puts "#{(CodeReview.joins(messages: :technical_words).where(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).pluck(:word).count).to_f/(CodeReview.where(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).count)}"
-	
-    puts "Average technical words in reviews for commits without bugs associated"
-    puts
-    puts "#{(CodeReview.joins(messages: :technical_words).where.not(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).pluck(:word).count).to_f/(CodeReview.where.not(commit_hash: CommitBug.pluck('DISTINCT commit_hash')).count)}"
-	
-    puts "Check increase of word usage over time"
-	
-    range0 = DateTime.parse('Tue, 5 Jan 1999 05:15:42 UTC +00:00')..Release.where(name: '5.0').pluck(:date)[0]
-    range1 = Release.where(name: '5.0').pluck(:date)[0]..Release.where(name: '11.0').pluck(:date)[0]
-    range2 = Release.where(name: '11.0').pluck(:date)[0]..Release.where(name: '19.0').pluck(:date)[0]
-    range3 = Release.where(name: '19.0').pluck(:date)[0]..Release.where(name: '27.0').pluck(:date)[0]
-    range4 = Release.where(name: '27.0').pluck(:date)[0]..Release.where(name: '35.0').pluck(:date)[0]
-	
-    ids = Participant.where(review_date: range0).pluck(:dev_id) + Reviewer.joins(:code_review).where(code_reviews: {created: range0}).pluck(:dev_id)
-	
-    old_words = Message.joins(:technical_words).where(date: range0, sender_id: ids).pluck('distinct word')
-    old_usage = Message.joins(:technical_words).where(date: range0, sender_id: ids).group(:sender_id, :word).pluck(:sender_id, :word)
-	
-    usage1 = Message.joins(:technical_words).where(messages: {date: range1, sender_id: ids},technical_words: {word: old_words}).group(:sender_id, :word).pluck(:sender_id, :word)
-    usage2 = Message.joins(:technical_words).where(messages: {date: range2, sender_id: ids},technical_words: {word: old_words}).group(:sender_id, :word).pluck(:sender_id, :word)
-    usage3 = Message.joins(:technical_words).where(messages: {date: range3, sender_id: ids},technical_words: {word: old_words}).group(:sender_id, :word).pluck(:sender_id, :word)
-    usage4 = Message.joins(:technical_words).where(messages: {date: range4, sender_id: ids},technical_words: {word: old_words}).group(:sender_id, :word).pluck(:sender_id, :word)
-	
-    puts
-    puts "New dev usage of words from 5.0 in 11.0: #{(usage1 - old_usage).count}"
-    puts "New dev usage of words from 5.0 in 19.0: #{(usage2 - usage1 - old_usage).count}"
-    puts "New dev usage of words from 5.0 in 27.0: #{(usage3 - usage1 - usage2 - old_usage).count}"
-    puts "New dev usage of words from 5.0 in 35.0: #{(usage4 - usage1 - usage2 - usage3 - old_usage).count}"
-=end
   end
   
+  def create_top_tech_words_tables
+    drop_top_msg_words = 'DROP TABLE IF EXISTS top_msg_words'										 
+    create_top_msg_words = 'CREATE TABLE top_msg_words AS (SELECT * FROM (SELECT label_id, word_id, weighttfidf, rank() OVER (PARTITION BY label_id ORDER BY weighttfidf DESC) AS rank FROM label_msg_top_words) t WHERE rank < 21)'
+	
+    drop_top_rev_words = 'DROP TABLE IF EXISTS top_rev_words'
+    create_top_rev_words = 'CREATE TABLE top_rev_words AS (SELECT * FROM (SELECT label_id, word_id, weighttfidf, rank() OVER (PARTITION BY label_id ORDER BY weighttfidf DESC) AS rank FROM label_rev_top_words) t WHERE rank < 21)'
+	
+    ActiveRecord::Base.connection.execute drop_top_msg_words
+    ActiveRecord::Base.connection.execute create_top_msg_words
+
+    ActiveRecord::Base.connection.execute drop_top_rev_words
+    ActiveRecord::Base.connection.execute create_top_rev_words
+  end
 end
