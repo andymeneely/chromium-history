513d60faf6ce8110d2c1bfefb3c7f8613492292f
Kayla Davis <kd9205@artifacts.gccis.rit.edu>
2015-05-14 18:54:23 -0400

    started to look into truck by removing dev 19

--- lib/analysis/createNetwork.py
+++ lib/analysis/createNetwork.py
@@ -41,13 +41,14 @@ while earlyBoundary < '2014-11-06 00:00:00.000000':
 		string = "SELECT * FROM adjacency_list WHERE review_date >= '" + earlyBoundary + "' AND review_date < '" + lateBoundary + "'";
 		cur.execute(string)
 		for row in cur:
-			# add each edge between two nodes, with the issue amd issue_owner
-			G.add_edge( row[1], row[2], issue=row[4], issue_owner=row[3] )
-			# add atrributes for whether or not this developer is experienced
-			G.node[row[1]]["sec_exp"] = row[6]
-			G.node[row[1]]["bugsec_exp"] = row[10]
-			G.node[row[2]]["sec_exp"] = row[7]
-			G.node[row[2]]["bugsec_exp"] = row[11]
+			if not row[1]==19 or row[2]==19: #remove this line if ou want to get data w/o truck factor on dev 19
+				# add each edge between two nodes, with the issue amd issue_owner
+				G.add_edge( row[1], row[2], issue=row[4], issue_owner=row[3] )
+				# add atrributes for whether or not this developer is experienced
+				G.node[row[1]]["sec_exp"] = row[6]
+				G.node[row[1]]["bugsec_exp"] = row[10]
+				G.node[row[2]]["sec_exp"] = row[7]
+				G.node[row[2]]["bugsec_exp"] = row[11]
 	except psycopg2.DatabaseError, e:
 		print 'Error %s' % e
 		sys.exit(1)
@@ -76,7 +77,7 @@ for gr in graphArray:
 	if num_nodes == 0:
 		continue
 	# move the node degree items into an ascending list of degrees for quartile ranges
-	sorted_deg = OrderedDict( sorted( node_deg.items(), key=lambda(k,v):(v,k) ) )
+	sorted_deg = OrderedDict( sorted( node_deg.items(), key=lambda(k,v):(v,k) ) )	
 	# print to screen if you want to see what's up in real time
 	#print "\nFor graph #" + str(grnum) + " IN TIME FRAME: " + gr.graph["begin"] + " UNTIL " + gr.graph["end"]
 
@@ -110,62 +111,10 @@ for gr in graphArray:
 		theFile.write(str(dev)+","+str(gr.node[dev]["degree"])+","+str(gr.node[dev]["centrality"])+","+str(gr.node[dev]["shr_hrs"])+","+str(gr.node[dev]["sec_exp"])+","+str(gr.node[dev]["bugsec_exp"])+","+str(gr.node[dev]["own_count"])+","+str(gr.graph["begin"])+","+str(gr.graph["end"])+"\n")
 		# print to screen to check values
 		#print "dev_id:%d, dev_deg:%d, centrality:%f, sher_hrs:%d, sec_exp:%s, bugsec_exp:%s, own_count:%d" %(dev, gr.node[dev]["degree"],gr.node[dev]["centrality"], gr.node[dev]["shr_hrs"], gr.node[dev]["sec_exp"], gr.node[dev]["bugsec_exp"], gr.node[dev]["own_count"])
+
 	grnum = grnum + 1	
 # Close all connections and files
 if con:
 	con.close()
 if theFile:
 	theFile.close()
-
-# THIS CODE IS NO LONGER USEFUL, WILL BE DELETED SOON
-#median = 0
-	#low_quart = 0
-	#upper_quart = 0
-	#outlier_quart = 0
-	#sorted_deg_values = sorted_deg.values()
-	#if ( num_nodes % 2 ) == 0:
-	#	mid_lo = (num_nodes / 2) - 1
-	#	mid_hi = num_nodes / 2
-	#	median = (sorted_deg_values[mid_lo] + sorted_deg_values[mid_hi])/2
-	#else:
-	#	median = sorted_deg_values[ ((num_nodes + 1) / 2)-1 ]
-	#low_mid = int( round( (num_nodes+1)/4 )-1 )
-	#low_quart = sorted_deg_values[low_mid]
-	#high_mid = int( round( (num_nodes-1)*0.75 ) )
-	#upper_quart = sorted_deg_values[high_mid]
-	#outlier_quart = upper_quart * 3
-#print "lower q:%d , median: %d, upper q:%d, outlier q:%d" %( low_quart, median, upper_quart, outlier_quart) 
-	# now group the developers by their degree values
-	#qrt1_devs = []
-	#qrt2_devs = []
-	#qrt3_devs = []
-	#qrt4_devs = []
-	#qrto_devs = []	
-	#	if sorted_deg[dev] <= low_quart:
-	#		#qrt1_devs.append(dev)
-	#		gr.node[dev]["qrtl"] = 1	
-	#	elif sorted_deg[dev] <= median:
-	#		#qrt2_devs.append(dev)
-	#		gr.node[dev]["qrtl"] = 2
-	#	elif sorted_deg[dev] <= upper_quart:
-	#		#qrt3_devs.append(dev)
-	#		gr.node[dev]["qrtl"] = 3
-	#	elif sorted_deg[dev] <= outlier_quart:
-	#		#qrt4_devs.append(dev)
-	#		gr.node[dev]["qrtl"] = 4
-	#	else:
-	#		#qrto_devs.append(dev) 
-	#		gr.node[dev]["qrtl"] = 5
-	#print "quartile 1:"
-	#for elem in qrt1_devs:
-	#	sys.stdout.write( str(elem) + "," )
-	#print "\nquartile 2:"
-	#for elem in qrt2_devs:
-	#	sys.stdout.write( str(elem) + "," )
-	#print "\nquartile 3:"
-	#for elem in qrt3_devs:
-	#	sys.stdout.write( str(elem) + "," )
-	# generate an ownership_count of issues this developer worked on
-	# during this specific time period
-	#for elem in qrto_devs:
-
