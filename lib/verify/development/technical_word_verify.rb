9c88e751ca5ead025415a69dbc7257b063b7d302
Andy Meneely <andy@se.rit.edu>
2015-09-03 15:33:15 -0400

    Fixing technical word verifies

--- lib/verify/development/technical_word_verify.rb
+++ lib/verify/development/technical_word_verify.rb
@@ -2,13 +2,13 @@ require_relative "../verify_base"
 
 class TechnicalWordVerify < VerifyBase
   def verify_word_count
-    assert_equal 236, TechnicalWord.count(:word)
+    assert_equal 71, TechnicalWord.count(:word)
   end
 
   def verify_word_associations
-    assert_equal 442, Message.joins(:technical_words).size
-    assert_equal 110, Commit.joins(:technical_words).size
-    assert_equal 44, CodeReview.joins(:technical_words).size
-    assert_equal 44132, AcmCategory.joins(:technical_words).size
+    assert_equal 74, Message.joins(:technical_words).size
+    assert_equal 26, Commit.joins(:technical_words).size
+    assert_equal 11, CodeReview.joins(:technical_words).size
+    assert_equal 7527, AcmCategory.joins(:technical_words).size
   end
 end
