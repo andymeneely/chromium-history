08caa3b45bbc9fcae7708dbbf2b40d0719563531
Samantha Oxley <sso7159@artifacts.gccis.rit.edu>
2015-09-22 11:52:53 -0400

    Getting rid of old files for developer snapshot, creating new files

---
+++ app/models/developer_snapshot.rb
@@ -0,0 +1,16 @@
+class DeveloperSnapshot < ActiveRecord::Base
+
+	def self.optimize
+		connection.add_index :developer_snapshots, :dev_id
+		connection.add_index :developer_snapshots, :degree
+		connection.add_index :developer_snapshots, :own_count
+		connection.add_index :developer_snapshots, :closeness
+		connection.add_index :developer_snapshots, :betweenness
+		connection.add_index :developer_snapshots, :sheriff_hrs
+		connection.add_index :developer_snapshots, :sec_exp
+		connection.add_index :developer_snapshots, :bugsec_exp
+		connection.add_index :developer_snapshots, :start_date
+		connection.add_index :developer_snapshots, :end_date
+		connection.execute 'CLUSTER developer_snapshots USING index_developer_snapshots_on_start'
+	end
+end
