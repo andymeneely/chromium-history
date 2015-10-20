93467f00cab4440e30dfeb2e2279bdba9e2eac5b
Samantha Oxley <sso7159@artifacts.gccis.rit.edu>
2015-10-20 09:54:31 -0400

    Removed some extra unnecessary steps in the old file

--- lib/analysis/devCollaboration.py
+++ lib/analysis/devCollaboration.py
@@ -22,12 +22,15 @@ def main():
 	# locate database files
 	username = sys.argv[1]
 	db = sys.argv[2]
-
+	
+	# establish connection with the database
 	data = est_connection(username, db)
 	# this will do every query necessary to build the bones of the graph
 	array = create_graph_array(data.cursor())
+	# after initial queries are complete, extrapolate some information
+	# from the structure 
 	finished_array = graph_extrapolate(array)
-	# here we simply push the data from the graph into the table
+	# now we simply push the data from the graph into the table
 	dev_graph(finished_array, data.cursor(), data)
 
 def est_connection(username, db):
@@ -53,24 +56,27 @@ def create_graph_array(cur):
 		thisGraph = nx.MultiGraph(begin=earlyBoundary, end=lateBoundary)
 		# query for this time boundary
 		try:
-			string = "SELECT * FROM adjacency_list WHERE review_date >= '" + earlyBoundary + "' AND review_date < '" + lateBoundary + "'";
+			string = "SELECT * FROM adjacency_list WHERE review_date >= '" + earlyBoundary 
+			string = string + "' AND review_date < '" + lateBoundary + "'";
+			
 			cur.execute(string)
 			for row in cur:
-				if not row[1]==19 or row[2]==19: #remove this line if ou want to get data w/o truck factor on dev 19
-					# add each edge between two nodes, with the issue amd issue_owner
-					thisGraph.add_edge( row[1], row[2], issue=row[4], issue_owner=row[3] )
-					# add atrributes for whether or not this developer is experienced
-					thisGraph.node[row[1]]["sec_exp"] = row[6]
-					thisGraph.node[row[1]]["bugsec_exp"] = row[10]
-					thisGraph.node[row[2]]["sec_exp"] = row[7]
-					thisGraph.node[row[2]]["bugsec_exp"] = row[11]
+				# add each edge between two nodes, with the issue amd issue_owner
+				thisGraph.add_edge( row[1], row[2], issue=row[4], issue_owner=row[3] )
+				# add atrributes for whether or not this developer is experienced
+				thisGraph.node[row[1]]["sec_exp"] = row[6]
+				thisGraph.node[row[1]]["bugsec_exp"] = row[10]
+				thisGraph.node[row[2]]["sec_exp"] = row[7]
+				thisGraph.node[row[2]]["bugsec_exp"] = row[11]
 		except psycopg2.DatabaseError, e:
 			print 'Error %s' % e
 			sys.exit(1)
 		# move the node degree items into an ascending list of degree values
 		for dev in nx.nodes(thisGraph):
 			# query for the developer's sheriff hours IN THIS TIME PERIOD
-			qry_shr_hrs = "SELECT dev_id, start, duration FROM sheriff_rotations WHERE dev_id =" + str(dev) + "AND start >= '" +earlyBoundary+ "' AND start < '" + lateBoundary + "'"
+			qry_shr_hrs = "SELECT dev_id, start, duration FROM sheriff_rotations WHERE dev_id =" 
+			qry_shr_hrs = qry_shr_hrs + str(dev) + "AND start >= '" +earlyBoundary+ "' AND start < '" + lateBoundary + "'"
+			
 			cur.execute(qry_shr_hrs) 
 			hrs_count = 0
 			for row in cur:
@@ -85,15 +91,15 @@ def create_graph_array(cur):
 	return graphArray
 
 def graph_extrapolate( graphArray ):
-	for graph in graphArray:	
+	for graph in graphArray:
+		# get dictionaries for all of these attributes per developer (node)	
 		node_deg = graph.degree()
 		closeness = nx.closeness_centrality(graph)
 		betweenness = nx.betweenness_centrality(graph)
-		
-		sorted_deg = OrderedDict( sorted( node_deg.items(), key=lambda(k,v):(v,k) ) )	
-		for dev in sorted_deg:
+
+		for dev in node_deg:
 			# we store degree and centrality as an attribute to the node 	
-			graph.node[dev]["degree"] = sorted_deg[dev]
+			graph.node[dev]["degree"] = node_deg[dev]
 			graph.node[dev]["closeness"] = round( closeness[dev], 4)
 			graph.node[dev]["betweenness"] = round( betweenness[dev], 8)
 			owner_count = 0
@@ -108,13 +114,14 @@ def graph_extrapolate( graphArray ):
 				if dev == edge[2]["issue_owner"]:
 					owner_count = owner_count + 1
 			graph.node[dev]["own_count"] = owner_count
+	return graphArray
 
-def dev_graph(gr, cur, con ):
+def dev_graph(graphArray, cur, con ):
 	# for each graph, let's categorize developers by their degree and begin
 	# to gather other useful information about them
 	cur.execute("delete from developer_snapshots")
-	for graph in gr:
-		for dev in nx.nodes(graph): 	
+	for gr in graphArray:
+		for dev in nx.nodes(gr): 	
 		# this should be writing into the database... 
 			cur.execute("INSERT INTO developer_snapshots( dev_id, degree, own_count, closeness,betweenness, sheriff_hrs, sec_exp, bugsec_exp, start_date, end_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (dev, gr.node[dev]["degree"], gr.node[dev]["own_count"], gr.node[dev]["closeness"],gr.node[dev]["betweenness"], gr.node[dev]["shr_hrs"], gr.node[dev]["sec_exp"],gr.node[dev]["bugsec_exp"], gr.graph["begin"], gr.graph["end"]) )
 		con.commit()
