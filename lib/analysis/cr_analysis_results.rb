36e5d1206bdffa4d15ad6a2b873628ebe82ababc
Kayla Nussbaum <kxn3867@rit.edu>
2016-04-24 15:56:31 -0400

    included print statements for spearman correlations

--- lib/analysis/cr_analysis_results.rb
+++ lib/analysis/cr_analysis_results.rb
@@ -74,5 +74,65 @@ class CRAnalysisResults
 			spearman_vMisses_sher <- cor(code_revs$vuln_misses, code_revs$total_sheriff_hours, method="spearman")
 			spearman_vMisses_churn <- cor(code_revs$vuln_misses, code_revs$churn, method="spearman")
 		EOR
+	
+		# print all the correlation results
+		puts "----------------------------------------------------------------------"
+		puts "--Spearman on non-participating-reviews VS totRevs/OwnGap/sher/churn--"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			non-participating reviews vs total reviews: #{R.pull("spearman_nonPR_tRevs")}
+			non-participating reviews vs owner gap: #{R.pull("spearman_nonPR_oFamGap")}
+			non-participating reviews vs sheriff hours: #{R.pull("spearman_nonPR_sher")}
+			non-participating reviews vs churn: #{R.pull("spearman_nonPR_churn")}
+		EOS
+		
+		puts "----------------------------------------------------------------------"
+		puts "----------Spearman on total reviews VS OwnGap/sher/churn--------------"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			total reviews with owners vs owner gap: #{R.pull("spearman_tRevs_oFamGap")}
+			total reviews with owners vs sheriff hours: #{R.pull("spearman_tRevs_sher")}
+			total reviews with owners vs churn: #{R.pull("spearman_tRevs_churn")}
+		EOS
+		
+		puts "----------------------------------------------------------------------"
+		puts "------Spearman on owner familiarity gap VS sheriff-hours/churn--------"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			owner familiarity gap vs sheriff hours: #{(R.pull("spearman_oFamGap_sher")}
+			owner familiarity gap vs churn: #{R.pull("spearman_oFamGap_churn")}
+		EOS
+
+		puts "----------------------------------------------------------------------"
+		puts "---------------Spearman on sheriff hours VS churn---------------------"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			sheriff hours vs churn: #{R.pull("spearman_sher_churn")}
+		EOS
+		
+		puts "----------------------------------------------------------------------"
+		puts "---Spearman on vuln missed VS non-partRevs/totRevs/OwnGap/sher/churn--"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			vuln missed vs non-participating reviews: #{R.pull("spearman_vMissed_nonPR")}
+			vuln missed vs total reviews with owners: #{R.pull("spearman_vMissed_tRevs")}
+			vuln missed vs owner familiarity gap: #{R.pull("spearman_vMissed_oFamGap")}
+			vuln missed vs sheriff hours: #{R.pull("spearman_vMissed_sher")}
+			vuln missed vs churn: #{R.pull("spearman_vMissed_churn")}
+		EOS
+
+		puts "----------------------------------------------------------------------"
+		puts "---Spearman on vuln misses VS non-partRevs/totRevs/OwnGap/sher/churn--"
+		puts "----------------------------------------------------------------------"
+		puts <<-EOS
+			vuln misses vs non-participating reviews: #{R.pull("spearman_vMisses_nonPR")}
+			vuln misses vs total reviews with owners: #{R.pull("spearman_vMisses_tRevs")}
+			vuln misses vs owner familiarity gap: #{R.pull("spearman_vMisses_oFamGap")}
+			vuln misses vs sheriff hours: #{R.pull("spearman_vMisses_sher")}
+			vuln misses vs churn: #{R.pull("spearman_vMisses_churn")}
+		EOS	
+
 	end
 		
+
+end
